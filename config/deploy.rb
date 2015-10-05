74315c15435ad54c6474ef577c4058d8a2996120
Matt Brictson <matt@mattbrictson.com>
2015-10-05 10:01:03 -0700

    Install rollbar

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,27 @@ set :mb_dotenv_keys, %w(
   rails_secret_key_base
   mandrill_username
   mandrill_api_key
+  rollbar_access_token
   sidekiq_web_username
   sidekiq_web_password
 )
+
+# Rollbar deployment notification
+task :notify_rollbar do
+  on release_roles(:all).first do
+    with fetch(:git_environmental_variables) do
+      within repo_path do
+        branch = fetch(:branch)
+        set :rollbar_sha, capture(:git, %(log #{branch} -n 1 --pretty=format:"%H"))
+        set :rollbar_token, capture("grep ROLLBAR_ACCESS_TOKEN #{shared_dotenv_path} | awk '{print substr($0, 22)}'")
+      end
+    end
+
+    revision = fetch(:rollbar_sha)
+    local_user = ENV["USER"]
+    rollbar_token = fetch(:rollbar_token)
+    rails_env = fetch(:rails_env)
+    execute "curl https://api.rollbar.com/api/1/deploy/ -F access_token=#{rollbar_token} -F environment=#{rails_env} -F revision=#{revision} -F local_username=#{local_user} >/dev/null 2>&1"
+  end
+end
+after "deploy:log_revision", "notify_rollbar"
