bd573b0c365bbc5bb8a76d4758635b073d2606d5
Matt Brictson <matt@mattbrictson.com>
2015-10-05 17:05:53 -0700

    Enable Devise + ActiveJob integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,4 +41,9 @@ class User < ActiveRecord::Base
   def authenticated?
     true
   end
+
+  # Devise + ActiveJob integration
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
