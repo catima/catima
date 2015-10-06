176771fdc231a8803c203adc8ad1f8d50af4c764
Matt Brictson <matt@mattbrictson.com>
2015-10-06 15:57:11 -0700

    Customize Devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,27 @@
 Rails.application.routes.draw do
   scope :path => ":locale" do
-    devise_for :users, :skip => [:sessions]
+    devise_for :users, :skip => [:passwords, :registrations, :sessions]
     devise_scope :user do
+      # passwords
+      get "forgot-password" => "devise/passwords#new", :as => :new_user_password
+      post "forgot-password" => "devise/passwords#create", :as => :user_password
+      get "change-password" => "devise/passwords#edit",
+          :as => :edit_user_password
+      patch "change-password" => "devise/passwords#update"
+
+      # registrations
+      get "register" => "devise/registrations#new",
+          :as => :new_user_registration
+      post "register" => "devise/registrations#create",
+          :as => :user_registration
+      get "my-profile" => "devise/registrations#edit",
+          :as => :edit_user_registration
+      patch "my-profile" => "devise/registrations#update"
+      get "cancel-account" => "devise/registrations#cancel",
+          :as => :cancel_user_registration
+      delete "cancel-account" => "devise/registrations#destroy"
+
+      # sessions
       get "login" => "devise/sessions#new", :as => :new_user_session
       post "login" => "devise/sessions#create", :as => :user_session
       delete "logout" => "devise/sessions#destroy", :as => :destroy_user_session
