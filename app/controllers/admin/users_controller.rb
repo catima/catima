58b4ad87ea1eff82a85d242078ab41b7ffb9eebb
Matt Brictson <matt@mattbrictson.com>
2015-10-07 11:42:06 -0700

    Implement admin/users#create

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,12 +3,31 @@ class Admin::UsersController < Admin::BaseController
 
   def new
     build_user
-    # TODO: authorize
+    # TODO: authorize(@user)
+  end
+
+  def create
+    build_user
+    # TODO: authorize(@user)
+    if @user.update(user_params)
+      redirect_to(admin_dashboard_path, :notice => user_created_message)
+    else
+      render("new")
+    end
   end
 
   private
 
   def build_user
     @user = User::CreateAdminForm.new(:invited_by => current_user)
+  end
+
+  def user_params
+    params.require(:user)
+      .permit(:email, :primary_language, :system_admin, :catalog_ids)
+  end
+
+  def user_created_message
+    "viim admin created! An invitation has been sent to #{@user.email}."
   end
 end
