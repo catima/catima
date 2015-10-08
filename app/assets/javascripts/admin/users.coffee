ec98409e1000e54f8b3b6cca776c92a25e81ddd9
Matt Brictson <matt@mattbrictson.com>
2015-10-08 15:16:52 -0700

    Clean up nested permissions code

--- app/assets/javascripts/admin/users.coffee
+++ app/assets/javascripts/admin/users.coffee
@@ -1,13 +1,16 @@
 init = ->
-  system_admin_box = $("#new_user #user_system_admin")
-  cat_boxes = $("#new_user input[type=checkbox][name='user[catalog_ids][]']")
+  system_admin_box = $("#user_system_admin")
+  cat_boxes = $("input[type=checkbox][name='user[catalog_ids][]']")
+  edit_permissions = $("[data-edit-permissions]")
 
   update_boxes = (evt)->
     if system_admin_box.get(0).checked
       cat_boxes.prop("checked", true)
       cat_boxes.attr("disabled", true)
+      edit_permissions.hide()
     else
       cat_boxes.attr("disabled", false)
+      edit_permissions.show()
 
   if system_admin_box.length > 0
     system_admin_box.on "change", update_boxes
--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,11 +1,46 @@
 module Admin::UsersHelper
   def admin_catalogs(user)
-    return content_tag(:span, "System", :class => "label label-primary") if user.system_admin?
-    user.admin_catalogs.map(&:name).to_sentence
+    if user.system_admin?
+      content_tag(:span, "System", :class => "label label-primary")
+    else
+      user.admin_catalogs.map(&:name).sort_by(&:downcase).to_sentence
+    end
   end
 
   def last_signed_in(user)
     at = [user.last_sign_in_at, user.current_sign_in_at].compact.max
     sentence_case("#{distance_of_time_in_words_to_now(at)} ago") unless at.nil?
+  end
+
+  def render_users_nested_permissions(form)
+    render(
+      :partial => "admin/users/nested_permissions",
+      :locals => {
+        :f => form,
+        :permissions => sorted_permissions_for_edit(form.object)
+      })
+  end
+
+  def render_users_role_button_bar(form)
+    roles = CatalogPermission::ROLE_OPTIONS.map do |role|
+      [role, form.object.role == role ? "active" : ""]
+    end
+    render(
+      :partial => "admin/users/role_button_bar",
+      :locals => {
+        :f => form,
+        :roles => roles
+      })
+  end
+
+  def sorted_permissions_for_edit(user)
+    Catalog.active.sorted.map do |catalog|
+      exist = user.catalog_permissions.find { |p| p.catalog_id == catalog.id }
+      exist || CatalogPermission.new(
+        :user => user,
+        :catalog => catalog,
+        :role => "user"
+      )
+    end
   end
 end
