92f49dc794c8070a7a0292094784fe78ac7bdf93
Matt Brictson <matt@mattbrictson.com>
2015-10-13 13:53:55 -0700

    Validate slug in ItemType

--- app/models/item_type.rb
+++ app/models/item_type.rb
@@ -11,10 +11,15 @@
 #
 
 class ItemType < ActiveRecord::Base
+  include HasSlug
+
   belongs_to :catalog
 
   validates_presence_of :catalog
   validates_presence_of :label
-  validates_presence_of :slug
-  validates_uniqueness_of :slug
+  validates_slug :scope => :catalog_id
+
+  def self.sorted
+    order("LOWER(item_types.label) ASC")
+  end
 end
--- test/models/item_type_test.rb
+++ test/models/item_type_test.rb
@@ -5,5 +5,7 @@ class ItemTypeTest < ActiveSupport::TestCase
   should validate_presence_of(:label)
   should validate_presence_of(:slug)
 
-  should validate_uniqueness_of(:slug)
+  should validate_uniqueness_of(:slug).scoped_to(:catalog_id)
+  should allow_value("hey").for(:slug)
+  should_not allow_value("under_score").for(:slug)
 end
