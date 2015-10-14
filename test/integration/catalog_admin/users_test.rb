dfc5907ccc56841f25dc773caa02640ce647f610
Matt Brictson <matt@mattbrictson.com>
2015-10-14 11:39:27 -0700

    Integration test for catalog user mgmt

---
+++ test/integration/catalog_admin/users_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class CatalogAdmin::UsersTest < ActionDispatch::IntegrationTest
+  test "create an editor" do
+    log_in_as("one-admin@example.com", "password")
+    visit("/one/admin/users")
+    click_on("New user")
+    fill_in("Email", :with => "testing@example.com")
+    choose("Editor")
+    select("Italiano", :from => "Preferred language")
+
+    assert_difference("User.count") do
+      click_on("Send invitation")
+    end
+
+    user = User.where(:email => "testing@example.com").first!
+    assert_equal([catalogs(:one).id], user.editor_catalog_ids)
+    assert_equal("it", user.primary_language)
+  end
+
+  test "edit a user" do
+    log_in_as("one-admin@example.com", "password")
+    visit("/one/admin/users")
+    first("a", :text => "Edit").click
+    click_on("Update user")
+    assert(page.has_content?("has been saved"))
+  end
+end
