e7b480bca830d6844ffc5140dd85eb712dc844bb
Matt Brictson <matt@mattbrictson.com>
2015-10-14 16:27:29 -0700

    Generate schema annotations

--- app/models/field/text.rb
+++ app/models/field/text.rb
@@ -1,3 +1,29 @@
+# == Schema Information
+#
+# Table name: fields
+#
+#  category_item_type_id :integer
+#  choice_set_id         :integer
+#  comment               :text
+#  created_at            :datetime         not null
+#  default_value         :text
+#  i18n                  :boolean          default(FALSE), not null
+#  id                    :integer          not null, primary key
+#  item_type_id          :integer
+#  multiple              :boolean          default(FALSE), not null
+#  name                  :string
+#  name_plural           :string
+#  options               :json
+#  ordered               :boolean          default(FALSE), not null
+#  position              :integer          default(0), not null
+#  related_item_type_id  :integer
+#  required              :boolean          default(TRUE), not null
+#  slug                  :string
+#  type                  :string
+#  unique                :boolean          default(FALSE), not null
+#  updated_at            :datetime         not null
+#
+
 class Field::Text < ::Field
   store_accessor :options, :maximum
   store_accessor :options, :minimum
