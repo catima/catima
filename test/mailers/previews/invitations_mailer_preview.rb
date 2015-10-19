77b3ce0e6c99628684687849f28870400d22822f
Matt Brictson <matt@mattbrictson.com>
2015-10-19 11:10:08 -0700

    Add i18n to mailer views, with en translations

--- test/mailers/previews/invitations_mailer_preview.rb
+++ test/mailers/previews/invitations_mailer_preview.rb
@@ -1,50 +1,59 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invitations_mailer
 class InvitationsMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/invitations_mailer/catalog_admin
-  def catalog_admin
-    catalog = Catalog.first_or_create!(
-      :name => "Sample",
-      :slug => "sample",
-      :primary_language => "en"
-    )
-    sender = User.new(:email => "ck@naxio.ch")
-    recipient = User.new(
-      :email => "matt@mattbrictson.com",
-      :invited_by => sender,
-      :system_admin => false,
-      :catalog_permissions => [
-        CatalogPermission.new(:catalog => catalog, :role => "admin")
-      ]
-    )
-    InvitationsMailer.admin(recipient, "__reset_password_token__")
-  end
+  # Use meta-programming to define multiple methods for each locale
+  %w(de en fr it).each do |locale|
+    # Preview this email at
+    # http://localhost:3000/rails/mailers/invitations_mailer/catalog_admin_en
+    define_method("catalog_admin_#{locale}") do
+      catalog = Catalog.first_or_create!(
+        :name => "Sample",
+        :slug => "sample",
+        :primary_language => "en"
+      )
+      sender = User.new(:email => "ck@naxio.ch")
+      recipient = User.new(
+        :email => "matt@mattbrictson.com",
+        :primary_language => locale,
+        :invited_by => sender,
+        :system_admin => false,
+        :catalog_permissions => [
+          CatalogPermission.new(:catalog => catalog, :role => "admin")
+        ]
+      )
+      InvitationsMailer.admin(recipient, "__reset_password_token__")
+    end
 
-  # Preview this email at http://localhost:3000/rails/mailers/invitations_mailer/system_admin
-  def system_admin
-    sender = User.new(:email => "ck@naxio.ch")
-    recipient = User.new(
-      :email => "matt@mattbrictson.com",
-      :invited_by => sender,
-      :system_admin => true
-    )
-    InvitationsMailer.admin(recipient, "__reset_password_token__")
-  end
+    # Preview this email at
+    # http://localhost:3000/rails/mailers/invitations_mailer/system_admin_en
+    define_method("system_admin_#{locale}") do
+      sender = User.new(:email => "ck@naxio.ch")
+      recipient = User.new(
+        :email => "matt@mattbrictson.com",
+        :primary_language => locale,
+        :invited_by => sender,
+        :system_admin => true
+      )
+      InvitationsMailer.admin(recipient, "__reset_password_token__")
+    end
 
-  # Preview this email at http://localhost:3000/rails/mailers/invitations_mailer/user
-  def user
-    catalog = Catalog.first_or_create!(
-      :name => "Sample",
-      :slug => "sample",
-      :primary_language => "en"
-    )
-    sender = User.new(:email => "ck@naxio.ch")
-    recipient = User.new(
-      :email => "matt@mattbrictson.com",
-      :invited_by => sender,
-      :catalog_permissions => [
-        CatalogPermission.new(:catalog => catalog, :role => "editor")
-      ]
-    )
-    InvitationsMailer.user(recipient, catalog, "__reset_password_token__")
+    # Preview this email at
+    # http://localhost:3000/rails/mailers/invitations_mailer/user_en
+    define_method("user_#{locale}") do
+      catalog = Catalog.first_or_create!(
+        :name => "Sample",
+        :slug => "sample",
+        :primary_language => "en"
+      )
+      sender = User.new(:email => "ck@naxio.ch")
+      recipient = User.new(
+        :email => "matt@mattbrictson.com",
+        :primary_language => locale,
+        :invited_by => sender,
+        :catalog_permissions => [
+          CatalogPermission.new(:catalog => catalog, :role => "editor")
+        ]
+      )
+      InvitationsMailer.user(recipient, catalog, "__reset_password_token__")
+    end
   end
 end
