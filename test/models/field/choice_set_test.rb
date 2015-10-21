88a4ce69b3a641a71408cde11e94aecb482d57ed
Matt Brictson <matt@mattbrictson.com>
2015-10-21 10:07:53 -0700

    Test Field::ChoiceSetPresenter#value

--- test/models/field/choice_set_test.rb
+++ test/models/field/choice_set_test.rb
@@ -5,7 +5,7 @@ class Field::ChoiceSetTest < ActiveSupport::TestCase
   should validate_presence_of(:choice_set)
 
   test "doesn't allow choice set from different catalog" do
-    choice_set_field = fields(:one_language)
+    choice_set_field = fields(:one_author_language)
     assert(choice_set_field.valid?)
 
     choice_set_field.choice_set = choice_sets(:two_languages)
---
+++ test/presenters/field/choice_set_presenter_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class Field::ChoiceSetPresenterTest < ActionView::TestCase
+  test "#value" do
+    author = items(:one_author_stephen_king)
+    english = choices(:one_english)
+    language_field = fields(:one_author_language)
+    # Have to set this manually because fixture doesn't know ID ahead of time
+    author.data["one_author_language_uuid"] = english.id
+
+    presenter = Field::ChoiceSetPresenter.new(self, author, language_field)
+    assert_equal("English", presenter.value)
+  end
+end
