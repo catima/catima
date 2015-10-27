211364f8279d92ce2e7039376287393205ef12d2
Matt Brictson <matt@mattbrictson.com>
2015-10-27 13:36:34 -0700

    Update integ. tests to work with i18n inputs

--- test/integration/catalog_admin/item_types_test.rb
+++ test/integration/catalog_admin/item_types_test.rb
@@ -5,8 +5,8 @@ class CatalogAdmin::ItemTypesTest < ActionDispatch::IntegrationTest
     log_in_as("two-admin@example.com", "password")
     visit("/two/admin")
     click_on("New item type")
-    fill_in("Name", :with => "Book")
-    fill_in("Name (plural)", :with => "Books")
+    fill_in("item_type[name_en]", :with => "Book")
+    fill_in("item_type[name_plural_en]", :with => "Books")
     fill_in("Slug (plural)", :with => "books")
 
     assert_difference("catalogs(:two).item_types.count") do
@@ -14,8 +14,8 @@ class CatalogAdmin::ItemTypesTest < ActionDispatch::IntegrationTest
     end
 
     type = catalogs(:two).item_types.where(:slug => "books").first!
-    assert_equal("Book", type.name)
-    assert_equal("Books", type.name_plural)
+    assert_equal("Book", type.name_primary)
+    assert_equal("Books", type.name_plural_primary)
   end
 
   test "edit an item type" do
@@ -23,13 +23,13 @@ class CatalogAdmin::ItemTypesTest < ActionDispatch::IntegrationTest
     visit("/two/admin")
     click_on("Author")
     click_on("Edit name and slug")
-    fill_in("Name", :with => "Writer")
+    fill_in("item_type[name_en]", :with => "Writer")
 
     assert_no_difference("catalogs(:two).item_types.count") do
       click_on("Save item type")
     end
 
     type = item_types(:two_author)
-    assert_equal("Writer", type.name)
+    assert_equal("Writer", type.name_primary)
   end
 end
