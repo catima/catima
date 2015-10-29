d3fdabe4c999913943b667eb0191ddf8a3b83ef0
Matt Brictson <matt@mattbrictson.com>
2015-10-29 16:58:37 -0700

    Integration tests for edit, delete, i18n fields

--- test/integration/catalog_admin/fields_test.rb
+++ test/integration/catalog_admin/fields_test.rb
@@ -1,23 +1,32 @@
 require "test_helper"
 
 class CatalogAdmin::FieldsTest < ActionDispatch::IntegrationTest
-  test "add a text field" do
-    log_in_as("two-admin@example.com", "password")
-    visit("/two/admin/authors/fields")
+  test "add a multilingual text field" do
+    log_in_as("multilingual-admin@example.com", "password")
+    visit("/multilingual/admin/authors/fields")
     click_on("Text field")
+
+    fill_in("field[name_de]", :with => "Geburtsort")
+    fill_in("field[name_plural_de]", :with => "Geburtsorte")
     fill_in("field[name_en]", :with => "Birthplace")
     fill_in("field[name_plural_en]", :with => "Birthplaces")
+    fill_in("field[name_fr]", :with => "Lieu de naissance")
+    fill_in("field[name_plural_fr]", :with => "Lieux de naissance")
+    fill_in("field[name_it]", :with => "Luogo di nascita")
+    fill_in("field[name_plural_it]", :with => "Luoghi di nascita")
+
     fill_in("Slug (singular)", :with => "birthplace")
     check("Use this as the primary field for Authors")
+    check("Enable i18n")
     select("Single value – required", :from => "field[style]")
     fill_in("Minimum length (optional)", :with => 3)
     fill_in("Maximum length (optional)", :with => 50)
 
-    assert_difference("item_types(:two_author).fields.count") do
+    assert_difference("item_types(:multilingual_author).fields.count") do
       click_on("Create field")
     end
 
-    field = item_types(:two_author).fields.where(:slug => "birthplace").first!
+    field = item_types(:multilingual_author).fields.where(:slug => "birthplace").first!
     assert_equal(3, field.minimum.to_i)
     assert_equal(50, field.maximum.to_i)
     assert_equal("Birthplace", field.name)
@@ -25,6 +34,7 @@ class CatalogAdmin::FieldsTest < ActionDispatch::IntegrationTest
     assert(field.display_in_list?)
     assert(field.primary?)
     assert(field.required?)
+    assert(field.i18n?)
     refute(field.multiple?)
   end
 
@@ -131,6 +141,30 @@ class CatalogAdmin::FieldsTest < ActionDispatch::IntegrationTest
 
     assert_difference("item_types(:two_author).fields.count") do
       click_on("Create field")
+    end
+  end
+
+  test "edit a field" do
+    log_in_as("one-admin@example.com", "password")
+    visit("/one/admin/authors/fields")
+    first("table a", :text => "Edit").click
+    fill_in("field[name_en]", :with => "Changed!")
+    fill_in("Slug", :with => "changed")
+
+    assert_no_difference("Field.count") do
+      click_on("Save field")
+    end
+
+    field = item_types(:one_author).fields.where(:slug => "changed").first!
+    assert_equal("Changed!", field.name)
+  end
+
+  test "delete a field" do
+    log_in_as("one-admin@example.com", "password")
+    visit("/one/admin/authors/fields")
+
+    assert_difference("item_types(:one_author).fields.count", -1) do
+      first("table a", :text => "Delete").click
     end
   end
 end
