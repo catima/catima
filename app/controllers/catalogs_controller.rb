0780c9a931c415456fe4c391f9940c634a36652e
Matt Brictson <matt@mattbrictson.com>
2015-10-30 09:26:10 -0700

    Move common catalog scoping logic into concern

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -1,41 +1,6 @@
 class CatalogsController < ApplicationController
+  include CatalogSite
+
   def show
-    find_active_catalog
-    redirect_to_specify_locale
-    remember_requested_locale
-  end
-
-  private
-
-  # TODO: move most of this functionality to base class or mixin,
-  # as it will be common to all catalog-scoped public-facing controllers.
-
-  attr_reader :catalog
-  helper_method :catalog
-
-  def catalog_scoped?
-    true
-  end
-
-  def redirect_to_specify_locale
-    return if @catalog.valid_locale?(params[:locale])
-    redirect_to(:locale => preferred_locale)
-  end
-
-  def preferred_locale
-    locales = [current_user.try(:primary_language)]
-    locales << @catalog.primary_language
-    locales.find { |l| @catalog.valid_locale?(l) }
-  end
-
-  def remember_requested_locale
-    return if params[:locale].nil?
-    return unless current_user.authenticated?
-    current_user.update_column(:primary_language, params[:locale])
-    true
-  end
-
-  def find_active_catalog
-    @catalog = Catalog.active.where(:slug => params[:catalog_slug]).first!
   end
 end
---
+++ app/controllers/concerns/catalog_site.rb
@@ -0,0 +1,42 @@
+# Behavior that is common to all catalog-scoped public controllers. Handles
+# loading the catalog and setting the locale.
+module CatalogSite
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :find_active_catalog
+    before_action :redirect_to_valid_locale
+    before_action :remember_requested_locale
+    helper_method :catalog
+  end
+
+  private
+
+  attr_reader :catalog
+
+  def catalog_scoped?
+    true
+  end
+
+  def find_active_catalog
+    @catalog = Catalog.active.where(:slug => params[:catalog_slug]).first!
+  end
+
+  def redirect_to_valid_locale
+    return if catalog.valid_locale?(params[:locale])
+    redirect_to(:locale => preferred_locale)
+  end
+
+  def preferred_locale
+    locales = [current_user.try(:primary_language)]
+    locales << catalog.primary_language
+    locales.find { |l| catalog.valid_locale?(l) }
+  end
+
+  def remember_requested_locale
+    return if params[:locale].nil?
+    return unless current_user.authenticated?
+    current_user.update_column(:primary_language, params[:locale])
+    true
+  end
+end
