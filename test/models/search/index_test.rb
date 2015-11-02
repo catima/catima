b362b498405ccda2c126b85744f728173064256b
Matt Brictson <matt@mattbrictson.com>
2015-11-02 15:28:57 -0800

    Make test insensitive to field ordering

--- test/models/search/index_test.rb
+++ test/models/search/index_test.rb
@@ -4,6 +4,8 @@ class Search::IndexTest < ActiveSupport::TestCase
   test "concatenates multiple keywords" do
     author = items(:one_author_stephen_king)
     data = Search::Index.new(:item => author, :locale => :en).data
-    assert_match(/Stephen King Steve/, data)
+    assert_instance_of(String, data)
+    assert_match(/Stephen King/, data)
+    assert_match(/Steve/, data)
   end
 end
