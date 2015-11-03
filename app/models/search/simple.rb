4bf1134cfeeded7c4f7f3a950996d6549f4449dd
Matt Brictson <matt@mattbrictson.com>
2015-11-02 17:10:58 -0800

    Implement simple search backend

---
+++ app/models/search/simple.rb
@@ -0,0 +1,30 @@
+class Search::Simple
+  attr_reader :catalog, :query
+
+  def initialize(catalog, query)
+    @catalog = catalog
+    @query = query
+  end
+
+  def sorted_items_by_type
+    return to_enum(__callee__) unless block_given?
+
+    found_type_ids = items.pluck("items.item_type_id").uniq
+
+    sorted_item_types.each do |type|
+      next unless found_type_ids.include?(type.id)
+      yield(type, type.sorted_items.merge(items))
+    end
+  end
+
+  def items
+    return Item.none if query.blank?
+    catalog.items.simple_search(query)
+  end
+
+  private
+
+  def sorted_item_types
+    catalog.item_types.includes(:fields).sorted
+  end
+end
