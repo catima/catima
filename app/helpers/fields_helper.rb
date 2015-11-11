fcda514e1a42f9224147de001e862691fe96f1bc
Matt Brictson <matt@mattbrictson.com>
2015-11-10 18:42:46 -0800

    Allow slug to be used to display a field value

--- app/helpers/fields_helper.rb
+++ app/helpers/fields_helper.rb
@@ -9,6 +9,14 @@ module FieldsHelper
   end
 
   def field_presenter(item, field, options={})
+    field = resolve_field(item, field)
     "#{field.class.name}Presenter".constantize.new(self, item, field, options)
+  end
+
+  def resolve_field(item, field_or_slug)
+    return field_or_slug if field_or_slug.is_a?(Field)
+    item.fields.find(-> { fail "Unknown field: #{field_or_slug}" }) do |f|
+      f.slug == field_or_slug.to_s
+    end
   end
 end
