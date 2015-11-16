074e523133b6a9ef88b491ab558bc31256feb396
Matt Brictson <matt@mattbrictson.com>
2015-11-16 14:08:33 -0800

    Add test for catalog-based view overrides

--- test/integration/items_test.rb
+++ test/integration/items_test.rb
@@ -44,4 +44,36 @@ class ItemsTest < ActionDispatch::IntegrationTest
       assert(page.has_content?("bio.doc"))
     end
   end
+
+  test "view item details with template override" do
+    author = items(:one_author_stephen_king)
+    with_customized_file("test/custom/items/show_author.html.erb",
+                         "catalogs/one/views/items/show.html+authors.erb") do
+      visit("/one/en/authors/#{author.to_param}")
+    end
+    assert(page.has_content?("This is a custom template"))
+    assert(page.has_content?("Stephen King"))
+    assert(page.has_content?("Steve"))
+    assert(page.has_content?("68"))
+    assert(page.has_content?("stephenking.com/index.html"))
+    assert(page.has_content?("sk@stephenking.com"))
+    assert(page.has_content?("1.88891"))
+    assert(page.has_content?("bio.doc"))
+  end
+
+  private
+
+  def with_customized_file(source, dest)
+    source = Rails.root.join(source)
+    dest_bak = Rails.root.join("#{dest}.bak")
+    dest = Rails.root.join(dest)
+
+    FileUtils.mkdir_p(dest.dirname)
+    FileUtils.mv(dest, dest_bak) if dest.file?
+    FileUtils.cp(source, dest)
+    yield
+  ensure
+    dest.unlink
+    FileUtils.mv(dest_bak, dest) if dest_bak.file?
+  end
 end
