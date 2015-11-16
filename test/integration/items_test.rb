5ae32af91fc376e3957b340315ecbf480d57e9b4
Matt Brictson <matt@mattbrictson.com>
2015-11-16 14:27:14 -0800

    Test layout customization

--- test/integration/items_test.rb
+++ test/integration/items_test.rb
@@ -61,6 +61,22 @@ class ItemsTest < ActionDispatch::IntegrationTest
     assert(page.has_content?("bio.doc"))
   end
 
+  test "view item details with custom layout" do
+    author = items(:one_author_stephen_king)
+    with_customized_file("test/custom/layouts/application.html.erb",
+                         "catalogs/one/views/layouts/application.html.erb") do
+      visit("/one/en/authors/#{author.to_param}")
+    end
+    assert(page.has_content?("This is a custom layout"))
+    assert(page.has_content?("Stephen King"))
+    assert(page.has_content?("Steve"))
+    assert(page.has_content?("68"))
+    assert(page.has_content?("stephenking.com/index.html"))
+    assert(page.has_content?("sk@stephenking.com"))
+    assert(page.has_content?("1.88891"))
+    assert(page.has_content?("bio.doc"))
+  end
+
   private
 
   def with_customized_file(source, dest)
