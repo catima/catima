945c524e6b3b510fbf71f9de452f4e563e87e925
Matt Brictson <matt@mattbrictson.com>
2015-11-16 17:15:06 -0800

    Integration test custom ERB views

---
+++ test/integration/custom_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class CustomTest < ActionDispatch::IntegrationTest
+  test "view a custom page by slug" do
+    with_customized_file("test/custom/custom/my-test-page.html.erb",
+                         "catalogs/one/views/custom/my-test-page.html.erb") do
+      visit("/one/en/my-test-page")
+    end
+    assert(page.has_content?("This is a custom page"))
+    assert(page.has_content?("Stephen King"))
+    assert(page.has_content?("Very Old"))
+  end
+end
--- test/integration/items_test.rb
+++ test/integration/items_test.rb
@@ -76,20 +76,4 @@ class ItemsTest < ActionDispatch::IntegrationTest
     assert(page.has_content?("1.88891"))
     assert(page.has_content?("bio.doc"))
   end
-
-  private
-
-  def with_customized_file(source, dest)
-    source = Rails.root.join(source)
-    dest_bak = Rails.root.join("#{dest}.bak")
-    dest = Rails.root.join(dest)
-
-    FileUtils.mkdir_p(dest.dirname)
-    FileUtils.mv(dest, dest_bak) if dest.file?
-    FileUtils.cp(source, dest)
-    yield
-  ensure
-    dest.unlink
-    FileUtils.mv(dest_bak, dest) if dest_bak.file?
-  end
 end
