20ab567180d04e2e6acece82cb360e8fec5b847d
Matt Brictson <matt@mattbrictson.com>
2015-11-16 17:14:47 -0800

    Provide custom pages with items based on type slug

--- app/controllers/custom_controller.rb
+++ app/controllers/custom_controller.rb
@@ -24,4 +24,16 @@ class CustomController < ApplicationController
   def show
     render(self.class.slug_to_view(params[:slug]))
   end
+
+  private
+
+  helper_method :items_of_type
+
+  def items_of_type(type_slug)
+    @items_of_type ||= {}
+    @items_of_type[type_slug] ||= begin
+      type = catalog.item_types.where(:slug => type_slug.to_s.downcase).first!
+      type.sorted_items
+    end
+  end
 end
