da4a3648472a8d9c088ce4a5b431d631c07cc1fa
Matt Brictson <matt@mattbrictson.com>
2015-11-18 14:17:49 -0800

    Impl #browse for ReferenceStrategy

--- app/models/search/reference_strategy.rb
+++ app/models/search/reference_strategy.rb
@@ -1,3 +1,6 @@
-# TODO
 class Search::ReferenceStrategy < Search::BaseStrategy
+  def browse(scope, item_id)
+    return scope.none if item_id.nil?
+    scope.where("#{data_field_expr} = ?", item_id)
+  end
 end
