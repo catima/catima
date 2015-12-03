07c69eae49cba4467c9d84c81e3d28715c20d384
Matt Brictson <matt@mattbrictson.com>
2015-12-02 17:09:06 -0800

    Work around malformed items JSON

--- app/models/external_type/client.rb
+++ app/models/external_type/client.rb
@@ -2,17 +2,24 @@ class ExternalType::Client
   NotFound = Class.new(StandardError)
 
   def get(uri)
-    conn.get(uri).body
+    parse_json_with_workaround(conn.get(uri).body)
   rescue Faraday::ResourceNotFound => e
     raise NotFound, e.message
   end
 
   private
 
+  # TODO: remove!
+  # This remove the malformed next/previous keys before parsing.
+  def parse_json_with_workaround(body)
+    JSON.parse(body.gsub(/"(next|previous)": [^\s",]+,/, ""))
+  end
+
   def conn
     @conn ||= Faraday.new do |c|
       c.response :raise_error
-      c.response :json, :content_type => /\bjson$/
+      # TODO: re-enable after removing above workaround
+      # c.response :json, :content_type => /\bjson$/
       c.response :logger, Rails.logger
       c.adapter Faraday.default_adapter
     end
