a4a7516aea3369ef76898d3597a388c60340e49f
Matt Brictson <matt@mattbrictson.com>
2015-12-04 16:49:20 -0800

    Generate schema doc

--- app/models/field/xref.rb
+++ app/models/field/xref.rb
@@ -1,3 +1,34 @@
+# == Schema Information
+#
+# Table name: fields
+#
+#  category_item_type_id    :integer
+#  choice_set_id            :integer
+#  comment                  :text
+#  created_at               :datetime         not null
+#  default_value            :text
+#  display_in_list          :boolean          default(TRUE), not null
+#  i18n                     :boolean          default(FALSE), not null
+#  id                       :integer          not null, primary key
+#  item_type_id             :integer
+#  multiple                 :boolean          default(FALSE), not null
+#  name_old                 :string
+#  name_plural_old          :string
+#  name_plural_translations :json
+#  name_translations        :json
+#  options                  :json
+#  ordered                  :boolean          default(FALSE), not null
+#  primary                  :boolean          default(FALSE), not null
+#  related_item_type_id     :integer
+#  required                 :boolean          default(TRUE), not null
+#  row_order                :integer
+#  slug                     :string
+#  type                     :string
+#  unique                   :boolean          default(FALSE), not null
+#  updated_at               :datetime         not null
+#  uuid                     :string
+#
+
 class Field::Xref < ::Field
   # TODO: support :only and :exclude options
 
