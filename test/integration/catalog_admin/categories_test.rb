b46fc0457b85b96d5bbfcd9a1286beac79fba574
Matt Brictson <matt@mattbrictson.com>
2015-12-07 11:43:45 -0800

    Integration test for create/edit categories

---
+++ test/integration/catalog_admin/categories_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class CatalogAdmin::CategoriesTest < ActionDispatch::IntegrationTest
+  test "create a category" do
+    log_in_as("two-admin@example.com", "password")
+    visit("/two/admin")
+    click_on("New category")
+    fill_in("Name", :with => "Bicycle")
+
+    assert_difference("catalogs(:two).categories.count") do
+      click_on("Create category")
+    end
+  end
+
+  test "edit a category" do
+    log_in_as("one-admin@example.com", "password")
+    visit("/one/admin")
+    click_on("Category One")
+    click_on("Edit name")
+    fill_in("Name", :with => "Edited by test")
+
+    assert_no_difference("catalogs(:two).categories.count") do
+      click_on("Save category")
+    end
+
+    category = categories(:one)
+    assert_equal("Edited by test", category.name)
+  end
+end
