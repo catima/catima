f75fa9a414d20bc5f1b32ef7520bc38290d59740
Matt Brictson <matt@mattbrictson.com>
2015-12-09 10:04:38 -0800

    Refactor adv search view into partials

---
+++ app/helpers/advanced_searches_helper.rb
@@ -0,0 +1,23 @@
+module AdvancedSearchesHelper
+  # Renders the contents of the `_#{field_type}_search_field.html.erb` partial
+  # and then yields it to the given block. If the partial doesn't render
+  # anything (i.e. it is empty), then we assume that field is not searchable.
+  # In this case the block is not rendered.
+  #
+  # This block rendering technique allows the template to specify wrapper
+  # markup (e.g. <tr> ... </tr>). The wrapper will only be rendered if the
+  # inner content is present.
+  #
+  def render_advanced_search_field(form, field, &block)
+    model_name = field.partial_name
+    partial = "advanced_searches/#{model_name}_search_field"
+    partial_rendered = ""
+    content = form.fields_for(:criteria) do |f|
+      f.fields_for(field.uuid) do |f|
+        partial_rendered = render(partial, :f => f, :field => field)
+      end
+    end
+    result = capture { block.call(content) }
+    strip_tags(partial_rendered).blank? ? nil : result
+  end
+end
