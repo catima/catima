aaed2e4525db34badab19f7eee2b30f4d139aeb3
Matt Brictson <matt@mattbrictson.com>
2016-01-04 10:26:06 -0800

    Test German and Italian date translations

--- test/presenters/field/date_time_presenter_test.rb
+++ test/presenters/field/date_time_presenter_test.rb
@@ -16,8 +16,14 @@ class Field::DateTimePresenterTest < ActionView::TestCase
     born_field = fields(:one_author_born)
     presenter = Field::DateTimePresenter.new(self, author, born_field)
 
+    I18n.with_locale(:de) do
+      assert_equal("21. September 1947", presenter.value)
+    end
     I18n.with_locale(:fr) do
       assert_equal("21 septembre 1947", presenter.value)
+    end
+    I18n.with_locale(:it) do
+      assert_equal("21 settembre 1947", presenter.value)
     end
   end
 
