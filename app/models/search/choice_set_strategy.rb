37b0e52e6546035c07801d69692bd7905e03a1bf
Matt Brictson <matt@mattbrictson.com>
2016-01-04 14:28:35 -0800

    Support multivalued choice sets in keyword search

--- app/models/search/choice_set_strategy.rb
+++ app/models/search/choice_set_strategy.rb
@@ -2,8 +2,8 @@ class Search::ChoiceSetStrategy < Search::BaseStrategy
   permit_criteria :any => []
 
   def keywords_for_index(item)
-    choice = field.selected_choice(item)
-    choice ? [choice.short_name, choice.long_name] : []
+    choices = field.selected_choices(item)
+    choices.flat_map { |choice| [choice.short_name, choice.long_name] }
   end
 
   def browse(scope, choice_slug)
