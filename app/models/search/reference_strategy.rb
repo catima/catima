b5ddf80c4d6e8018fa46753edb703f208726c4d6
Matt Brictson <matt@mattbrictson.com>
2016-01-05 14:15:39 -0800

    Implement browse for multi-valued ref fields

--- app/models/search/reference_strategy.rb
+++ app/models/search/reference_strategy.rb
@@ -1,6 +1,19 @@
 class Search::ReferenceStrategy < Search::BaseStrategy
   def browse(scope, item_id)
     return scope.none if item_id.nil?
-    scope.where("#{data_field_expr} = ?", item_id)
+
+    # TODO: DRY up with ChoiceSetStrategy
+    if field.multiple?
+      scope.where("#{data_field_jsonb_expr} ?| array[:ids]", :ids => [item_id])
+    else
+      scope.where("#{data_field_expr} = ?", item_id)
+    end
+  end
+
+  private
+
+  # TODO: DRY up with ChoiceSetStrategy
+  def data_field_jsonb_expr
+    "(items.data->'#{field.uuid}')::jsonb"
   end
 end
