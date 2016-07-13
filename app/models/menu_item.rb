47d69fded662bd02fbf84a902e9dd39cea2ce0c5
Christian Kaiser <ck@naxio.ch>
2016-07-13 09:34:07 +0200

    Implemented submenus in catalog menu bar

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -23,8 +23,28 @@ class MenuItem < ActiveRecord::Base
   validates_presence_of :catalog
   validates_presence_of :title
 
+  include Rails.application.routes.url_helpers
+
 
   def self.sorted
     order("menu_items.rank ASC")
+  end
+
+  def submenus
+    return nil if parent_id   # we don't have nested menus
+    MenuItem.where(parent_id: id).order("menu_items.rank ASC")
+  end
+
+  def get_url(locale=nil)
+    locale = catalog.primary_language if locale.nil?
+    if not item_type.nil?
+      items_path(catalog, locale, item_type.slug)
+    elsif not page.nil?
+      page_path(catalog, locale, page)
+    elsif not url.empty?
+      url
+    else
+      '#'
+    end
   end
 end
