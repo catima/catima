4b4f2899c2b8559f2068ade9aeeb466507c95dde
Christian Kaiser <ck@naxio.ch>
2016-07-20 11:04:59 +0200

    Container updating

--- app/controllers/catalog_admin/containers_controller.rb
+++ app/controllers/catalog_admin/containers_controller.rb
@@ -6,11 +6,54 @@ class CatalogAdmin::ContainersController < CatalogAdmin::BaseController
     authorize(@container)
   end
 
+  def update
+    find_container
+    authorize(@container)
+    if @container.update(container_params)
+      respond_to do |f|
+        f.js
+        f.html do
+          redirect_to(
+            edit_catalog_admin_page_path(@container.page.catalog, @container.page),
+            :notice => updated_message
+          )
+        end
+      end
+    else
+      render('edit')
+    end
+  end
+
   private
+
+  def container_class
+    Container::TYPES.fetch(params[:type], "Container::HTML").constantize
+  end
 
   def find_container
     @container = nil
     c = Container.find(params[:id])
     @container = c if catalog.id == c.page.catalog.id
   end
+
+  def container_params
+    params.require(:container).permit(
+      :slug,
+      :row_order_position,
+      *@container.custom_container_permitted_attributes
+    )
+  end
+
+  def created_message
+    "The “#{@container.slug}” container has been created."
+  end
+
+  def updated_message
+    "The “#{@container.slug}” container has been saved."
+  end
+
+  def destroyed_message
+    "The “#{@container.slug}” container has been deleted."
+  end
+
 end
