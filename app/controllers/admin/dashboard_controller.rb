b5e2ec01214f2017417965d80fecf16d7718267f
Christian Kaiser <ck@naxio.ch>
2016-12-24 11:12:36 +0100

    Admin route for catalog admins

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,11 +1,19 @@
 class Admin::DashboardController < Admin::BaseController
   def index
-    authorize(Catalog, :index?)
-    authorize(User, :index?)
+    if current_user.system_admin?
+      authorize(Catalog, :index?)
+      authorize(User, :index?)
 
-    @users = User.sorted
-    @catalogs = Catalog.sorted
-    @configuration = ::Configuration.first!
-    @template_storages = TemplateStorage.all
+      @users = User.sorted
+      @catalogs = Catalog.sorted
+      @configuration = ::Configuration.first!
+      @template_storages = TemplateStorage.all
+    else
+      @catalogs = Catalog.sorted.select do |catalog|
+        catalog_access = current_user.catalog_role_at_least? catalog, "editor"
+        authorize(catalog, :show?) if catalog_access
+        catalog_access
+      end
+    end
   end
 end
