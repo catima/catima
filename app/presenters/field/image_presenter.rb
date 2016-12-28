1183267955a1c5236e66a1776626a386b9dcab37
Christian Kaiser <ck@naxio.ch>
2016-12-28 08:53:38 +0100

    Enabled thumb creation for image fields

--- app/presenters/field/image_presenter.rb
+++ app/presenters/field/image_presenter.rb
@@ -4,18 +4,22 @@ class Field::ImagePresenter < Field::FilePresenter
   def value
     return nil if raw_value.nil?
     options[:class] = options[:style] ? options[:style] : :full
+    transform = case options[:class]
+    when :compact then [:fill, '100x100']
+    when :medium then [:fill, '250x250']
+    else [:resize, '600x600']
+    end
     images = files_as_array.map do |image|
-      image_tag(file_url(image), options.merge(self.options))
+      image_tag(file_url(image, transform[1], transform[0]), options.merge(self.options))
     end
     images.join(' ').html_safe
   end
 
-  private
-
-  # TODO: transform image
-  #   case options[:style]
-  #   when :compact then [:fill, 64, 64]
-  #   when :medium then [:fill, 250, 250]
-  #   else [:limit, 600, 600]
-  #   end
+  def file_url(file, size=nil, mode=:fill)
+    return nil if file['path'].nil?
+    return "/#{file['path']}" if size.nil?
+    path_parts = file['path'].split('/')
+    "/thumbs/#{path_parts[1]}/#{size}/#{mode}/#{path_parts[2]}/#{path_parts[3]}"
+  end
+  
 end
