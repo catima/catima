0d69aa33bf3390356ce6a4354628cbfcba749fcb
Matt Brictson <matt@mattbrictson.com>
2017-01-29 16:15:50 -0800

    Switch to explicit _links attribute

--- app/serializers/api/v1/catalog_serializer.rb
+++ app/serializers/api/v1/catalog_serializer.rb
@@ -1,8 +1,14 @@
 class API::V1::CatalogSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+
   attributes :id, :name, :primary_language, :other_languages, :slug
   attribute(:advertize) { object.advertize? }
 
-  link(:self) { api_v1_catalog_url(object.slug) }
-  link(:pages) { api_v1_catalog_pages_url(object.slug) }
-  link(:html) { catalog_home_url(object.slug) }
+  attribute("_links") do
+    {
+      :self => api_v1_catalog_url(object.slug),
+      :pages => api_v1_catalog_pages_url(object.slug),
+      :html => catalog_home_url(object.slug)
+    }
+  end
 end
--- app/serializers/api/v1/page_serializer.rb
+++ app/serializers/api/v1/page_serializer.rb
@@ -1,15 +1,11 @@
 class API::V1::PageSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+
   attributes :id, :slug, :title, :locale
-
-  link(:self) do
-    api_v1_catalog_page_url(object.id, :catalog_slug => object.catalog.slug)
-  end
-
-  link(:html) do
-    page_url(
-      object.slug,
-      :catalog_slug => object.catalog.slug,
-      :locale => object.locale
-    )
+  attribute(:_links) do
+    {
+      :self => api_v1_catalog_page_url(object.id, :catalog_slug => object.catalog.slug),
+      :html => page_url(object.slug, :catalog_slug => object.catalog.slug, :locale => object.locale)
+    }
   end
 end
