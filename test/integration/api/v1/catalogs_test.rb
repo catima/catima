8b31bf49ea4b3675fa1b01741682faaa4cb82822
Matt Brictson <matt@mattbrictson.com>
2017-04-23 11:52:56 -0700

    Add pagination to catalogs API endpoint

--- test/integration/api/v1/catalogs_test.rb
+++ test/integration/api/v1/catalogs_test.rb
@@ -8,9 +8,19 @@ class API::V1::CatalogsTest < ActionDispatch::IntegrationTest
     assert_response_schema("v1/catalogs.json")
   end
 
+  test "GET catalogs honors page size" do
+    get("/api/v1/catalogs?page_size=1")
+    assert_equal(1, json_response["page_size"])
+    assert_equal(1, json_response["catalogs"].size)
+
+    get("/api/v1/catalogs?page_size=2")
+    assert_equal(2, json_response["page_size"])
+    assert_equal(2, json_response["catalogs"].size)
+  end
+
   test "GET catalogs includes valid links" do
     get("/api/v1/catalogs")
-    catalog = json_response.first
+    catalog = json_response["catalogs"].first
     assert_equal(
       "http://localhost:3000/api/v1/catalogs/#{catalog['slug']}",
       catalog["_links"]["self"]
@@ -23,7 +33,7 @@ class API::V1::CatalogsTest < ActionDispatch::IntegrationTest
 
   test "GET catalogs doesn't include inactive catalogs" do
     get("/api/v1/catalogs")
-    assert_nil(json_response.find { |c| c["slug"] == "inactive" })
+    assert_nil(json_response["catalogs"].find { |c| c["slug"] == "inactive" })
   end
 
   test "GET catalog conforms to JSON schema" do
