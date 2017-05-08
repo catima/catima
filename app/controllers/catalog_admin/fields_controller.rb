e4782eed201f99263183e088f3d713df04a842b1
Matt Brictson <matt@mattbrictson.com>
2017-05-07 17:07:20 -0700

    Allow UI component to be set via admin UI

--- app/controllers/catalog_admin/fields_controller.rb
+++ app/controllers/catalog_admin/fields_controller.rb
@@ -81,6 +81,7 @@ class CatalogAdmin::FieldsController < CatalogAdmin::BaseController
       :display_in_list,
       :i18n,
       :row_order_position,
+      :ui_component,
       *@field.custom_field_permitted_attributes
     )
   end
