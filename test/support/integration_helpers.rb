9400fbbd2bf896716775a134f934ef6e2b91637e
Matt Brictson <matt@mattbrictson.com>
2017-05-16 08:34:54 -0700

    Reorganize test files
    Also move `with_customized_file` into a support module so that we can
    use it outside of integration tests.

--- test/support/integration_helpers.rb
+++ test/support/integration_helpers.rb
@@ -1,4 +1,6 @@
 class ActionDispatch::IntegrationTest
+  include CustomizedFiles
+
   private
 
   def fill_in_hidden(id, with:)
@@ -10,19 +12,5 @@ class ActionDispatch::IntegrationTest
     fill_in("Email", :with => email)
     fill_in("Password", :with => password)
     within("form") { click_on("Log in") }
-  end
-
-  def with_customized_file(source, dest)
-    source = Rails.root.join(source)
-    dest_bak = Rails.root.join("#{dest}.bak")
-    dest = Rails.root.join(dest)
-
-    FileUtils.mkdir_p(dest.dirname)
-    FileUtils.mv(dest, dest_bak) if dest.file?
-    FileUtils.cp(source, dest)
-    yield
-  ensure
-    dest.unlink
-    FileUtils.mv(dest_bak, dest) if dest_bak.file?
   end
 end
