3968bba934edc4f7f150a8c64913b188e37fab19
Christian Kaiser <ck@naxio.ch>
2017-08-22 15:14:39 +0200

    Default locale set to en for all environments

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,5 @@
 class Admin::BaseController < ApplicationController
   layout "admin"
-  skip_before_action :set_locale
   before_action :authenticate_user!
   after_action :verify_authorized
 
--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,7 +1,7 @@
 # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
 # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
 
-Rails.application.config.i18n.default_locale = Rails.env.test? ? :en : :fr
+Rails.application.config.i18n.default_locale = :en
 Rails.application.config.i18n.available_locales = %i(de en fr it)
 
 # TODO: uncomment once Catalog behavior is implemented
