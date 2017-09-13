7e60f566f531bb6798afbc3a07b7b3ed546b5d57
Jason Urton <jason@codingbrackets.com>
2017-09-13 11:48:33 +0200

    Loading of categories from dump

--- app/services/catalog_load.rb
+++ app/services/catalog_load.rb
@@ -6,15 +6,18 @@ class CatalogLoad
 
   def load
     raise "ERROR. '#{@load_dir}' is not a directory." unless File.directory?(@load_dir)
-    meta = get_meta
-    raise "ERROR. Dump version '#{meta['dump_version']}' is not supported." unless meta['dump_version'] == '1.0'
+    m = meta
+    raise "ERROR. Dump version '#{meta['dump_version']}' is not supported." unless m['dump_version'] == '1.0'
     File.open(File.join(@load_dir, 'structure', 'catalog.json')) do |f|
       create_catalog(JSON.parse(f.read))
     end
+    load_structure
   end
 
+  private
+
   # Gets the meta information
-  def get_meta
+  def meta
     File.open(File.join(@load_dir, 'meta.json')) do |f|
       JSON.parse(f.read)
     end
@@ -24,8 +27,32 @@ class CatalogLoad
   def create_catalog(catalog_info)
     @slug ||= catalog_info['slug']
     # Check if such a catalog already exists
-    catalog = Catalog.find_by({slug: @slug})
+    catalog = Catalog.find_by(slug: @slug)
     raise "ERROR. Catalog '#{@slug}' already exists. Please specify a unique catalog slug." unless catalog.nil?
-    Catalog.new(catalog_info.merge({slug: @slug})).save
+    Catalog.new(catalog_info.merge(slug: @slug)).save
+  end
+
+  def load_structure
+    catalog = Catalog.find_by(slug: @slug)
+
+    # Load first the categories (they don't have any dependency)
+    load_categories(catalog, File.join(@load_dir, 'structure', 'categories.json'))
+
+    # Load the choice sets
+    # Load the item types
+  end
+
+  def load_categories(catalog, categories_file)
+    return unless File.exist?(categories_file)
+    File.open(categories_file) do |f|
+      JSON.parse(f.read)['categories'].each { |cat| load_category(catalog, cat) }
+    end
+  end
+
+  def load_category(catalog, category_info)
+    category = catalog.categories.build(category_info.slice("name", "uuid").merge(
+                                          created_at: DateTime.current, updated_at: DateTime.current))
+    category.save
+    category_info['fields'].each { |fld_info| category.fields.build(fld_info).save }
   end
 end
--- lib/tasks/catalog/load.rake
+++ lib/tasks/catalog/load.rake
@@ -1,7 +1,7 @@
 namespace :catalog do
   desc "Load catalog from dump directory"
-  LOAD_USAGE = 'rake catalog:load dir=<dump-directory> slug=<new-slug>'
-  task :load => [:environment] do 
+  LOAD_USAGE = 'rake catalog:load dir=<dump-directory> slug=<new-slug>'.freeze
+  task :load => [:environment] do
     (directory = ENV['dir']) || raise("No dump directory specified. \n\rUSAGE: #{LOAD_USAGE}")
     new_slug = ENV['slug']
     CatalogLoad.new(directory, new_slug).load
