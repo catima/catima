1b50ca4da1825e098db73f51636a084974e3090f
Jason Urton <jason@codingbrackets.com>
2017-12-04 10:16:40 +0100

    Migrated from DraftJS to Summernote for template editor

--- app/javascript/bundles/TemplateEditor/components/TemplateEditor.jsx
+++ app/javascript/bundles/TemplateEditor/components/TemplateEditor.jsx
@@ -1,307 +1,121 @@
 import 'es6-shim';
 import PropTypes from 'prop-types';
 import React from 'react';
-import {
-  CompositeDecorator,
-  Editor,
-  EditorState,
-  Modifier,
-  RichUtils,
-} from 'draft-js';
-import {
-  convertFromHTML,
-  convertToHTML,
-} from 'draft-convert';
 
+const uuidv4 = require('uuid/v4');
 
 
 class TemplateEditor extends React.Component {
   static propTypes = {
     contentRef: PropTypes.string.isRequired,
     locale: PropTypes.string.isRequired,
+    fields: PropTypes.array.isRequired,
   };
 
   constructor(props){
     super(props);
-    const self = this;
-
-    const compositeDecorator = new CompositeDecorator([
-      {
-        strategy: fieldStrategy,
-        component: FieldSpan,
+    this.uid = `summernote-${uuidv4()}`;
+    this.updateContent = this._updateContent.bind(this);
+    this.options = {
+      minHeight: 150,
+      toolbar: [
+        ['style', ['bold', 'italic', 'underline', 'clear']],
+        ['font', ['strikethrough', 'superscript', 'subscript']],
+        ['fontsize', ['fontsize']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['height', ['height']],
+        ['insert', ['picture', 'link', 'table', 'hr']],
+        ['misc', ['codeview']],
+        ['templateEditor', ['fieldsMenu', 'itemLinkButton']]
+      ],
+      buttons: {
+        fieldsMenu: this.fieldsMenu(this.props.fields),
+        itemLinkButton: this.itemLinkButton(),
       },
-    ]);
-
-    this.state = {
-      editorState: EditorState.createWithContent(
-        convertFromHTML(this._loadContent(this.props.contentRef)),
-        compositeDecorator
-      ),
+      callbacks: {
+        onChange: this.updateContent
+      }
     };
-    this.focus = () => this.refs.editor.focus();
-    this.onChange = function(editorState){
-      self._updateContent(self.props.contentRef, convertToHTML(editorState.getCurrentContent()));
-      self.setState({editorState});
-    }
-    this.handleKeyCommand = this._handleKeyCommand.bind(this);
-    this.onTab = this._onTab.bind(this);
-    this.toggleBlockType = this._toggleBlockType.bind(this);
-    this.toggleInlineStyle = this._toggleInlineStyle.bind(this);
-    this.addField = this._addField.bind(this);
-    this.onFieldChange = this._confirmField.bind(this);
-    this.onFieldInputKeyDown = this._onFieldInputKeyDown.bind(this);
   }
 
-  _handleKeyCommand(command, editorState) {
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return 'handled';
-    }
-    return 'not-handled';
-  }
-
-  _onTab(e) {
-    const maxDepth = 4;
-    this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth));
-  }
-
-  _toggleBlockType(blockType) {
-    this.onChange(RichUtils.toggleBlockType(this.state.editorState, blockType));
-  }
-
-   _toggleInlineStyle(inlineStyle) {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, inlineStyle));
-  }
-
-  _loadContent(ref){
-    let v = JSON.parse(document.getElementById(ref).value || {});
+  _loadContent(){
+    let v = JSON.parse(document.getElementById(this.props.contentRef).value || {});
     return v[this.props.locale] || '';
   }
 
-  _updateContent(ref, html){
-    let v = JSON.parse(document.getElementById(ref).value || {});
+  _updateContent(html){
+    const el = document.getElementById(this.props.contentRef)
+    let v = JSON.parse(el.value || {});
     v[this.props.locale] = html;
-    document.getElementById(ref).value = JSON.stringify(v);
+    el.value = JSON.stringify(v);
   }
 
-  _addField(e){
-    e.preventDefault();
-    const {editorState, fieldName} = this.state;
-    this.setState({
-      showFieldInput: true,
-      fieldName: fieldName,
-    }, () => {
-      setTimeout(() => this.refs.fieldName.focus(), 0);
-    });
+  componentDidMount(){
+    const html = this._loadContent();
+    this.editor = $(`#${this.uid}`);
+    this.editor.summernote(this.options);
+    this.editor.summernote('code', html);
+    $('.note-link-popover').css('display', 'none');
   }
 
-  _onFieldInputKeyDown(e){
-    if (e.which === 27) {
-      this.setState({
-        showFieldInput: false,
-        fieldName: '',
+  componentWillUnmount(){
+    this.editor.summernote('destroy');
+  }
+
+  fieldsMenu(fields){
+    return function(context){
+      const ui = $.summernote.ui;
+      let fieldsMenu = [];
+      for (let i in fields){
+        let f = fields[i];
+        fieldsMenu.push(`<${f.type}Field>${f.slug}</${f.type}Field>`);
+      }
+      const button = ui.buttonGroup([
+        ui.button({
+          className: 'dropdown-toggle',
+          contents: 'Add field <i class="fa fa-caret-down"></i>',
+          data: {
+            toggle: 'dropdown'
+          }
+        }),
+        ui.dropdown({
+          className: 'dropdown-fontsize',
+          items: fieldsMenu,
+          click: function(e){
+            e.preventDefault();
+            const fieldSlug = $(e.target).text();
+            context.invoke('editor.insertText', ' {{'+fieldSlug+'}} ');
+          },
+        })
+      ]);
+      return button.render();
+    };
+  }
+
+  itemLinkButton(){
+    return function(context) {
+      const ui = $.summernote.ui;
+      const button = ui.button({
+        contents: 'Item link',
+        click: function(){
+          const range = context.invoke('editor.createRange');
+          let selectedText = range.toString();
+          if (selectedText.length < 1) selectedText = 'item-link';
+          context.invoke('editor.createLink', {text: selectedText, url: '{{_itemLink}}'});
+        }
       });
-    }
-  }
-
-  _confirmField(e){
-    e.preventDefault();
-    let fieldName = e.target.value;
-    const {editorState} = this.state;
-    const selection = editorState.getSelection();
-    const contentState = editorState.getCurrentContent();
-    const ncs = Modifier.insertText(contentState, selection, " {{"+fieldName+"}} ");
-    const es = EditorState.push(editorState, ncs, 'insert-fragment');
-    this.setState({
-      editorState: es,
-      showFieldInput: false,
-      fieldName: '',
-    }, () => {
-      setTimeout(() => this.refs.editor.focus(), 0);
-    });
+      return button.render();
+    };
   }
 
   render(){
-    const {editorState} = this.state;
-    let className = 'RichEditor-editor';
-
-    let fieldInput;
-    if (this.state.showFieldInput){
-      fieldInput =
-        <div style={styles.fieldInputContainer}>
-          <select
-            onChange={this.onFieldChange}
-            ref="fieldName"
-            style={styles.fieldInput}
-            type="text"
-            value={this.state.fieldName}
-            onKeyDown={this.onFieldInputKeyDown}
-            className="form-control templateEditorFieldSelect"
-          >
-            <option value="">---</option>
-            {this.props.fields.map((f) =>
-              <option value={f.slug}>{f.name}</option>
-            )}
-          </select>
-        </div>
-    }
-
     return (
-      <div className="RichEditor-root">
-        <BlockStyleControls
-          editorState={editorState}
-          onToggle={this.toggleBlockType}
-        />
-        <InlineStyleControls
-          editorState={editorState}
-          onToggle={this.toggleInlineStyle}
-        />
-        <div className="RichEditor-controls">
-          <span className="RichEditor-styleButton" onMouseDown={this.addField} style={{marginRight: 10}}>Add field</span>
-          {fieldInput}
-        </div>
-        <div className={className} onClick={this.focus}>
-          <Editor
-            blockStyleFn={getBlockStyle}
-            editorState={editorState}
-            onChange={this.onChange}
-            handleKeyCommand={this.handleKeyCommand}
-            onTab={this.onTab}
-            ref="editor"
-          />
-        </div>
+      <div className="templateEditor">
+        <div id={this.uid}></div>
       </div>
     );
   }
 }
 
-function getBlockStyle(block) {
-  switch (block.getType()) {
-    case 'blockquote': return 'RichEditor-blockquote';
-    default: return null;
-  }
-}
-
-class StyleButton extends React.Component {
-  constructor() {
-    super();
-    this.onToggle = (e) => {
-      e.preventDefault();
-      this.props.onToggle(this.props.style);
-    };
-  }
-  render() {
-    let className = 'RichEditor-styleButton';
-    if (this.props.active) {
-      className += ' RichEditor-activeButton';
-    }
-    return (
-      <span className={className} onMouseDown={this.onToggle}>
-        {this.props.label}
-      </span>
-    );
-  }
-}
-
-const BLOCK_TYPES = [
-  {label: 'H1', style: 'header-one'},
-  {label: 'H2', style: 'header-two'},
-  {label: 'H3', style: 'header-three'},
-  {label: 'H4', style: 'header-four'},
-  {label: 'H5', style: 'header-five'},
-  {label: 'H6', style: 'header-six'},
-  {label: 'UL', style: 'unordered-list-item'},
-  {label: 'OL', style: 'ordered-list-item'},
-];
-
-const BlockStyleControls = (props) => {
-  const {editorState} = props;
-  const selection = editorState.getSelection();
-  const blockType = editorState
-    .getCurrentContent()
-    .getBlockForKey(selection.getStartKey())
-    .getType();
-  return (
-    <div className="RichEditor-controls">
-      {BLOCK_TYPES.map((type) =>
-        <StyleButton
-          key={type.label}
-          active={type.style === blockType}
-          label={type.label}
-          onToggle={props.onToggle}
-          style={type.style}
-        />
-      )}
-    </div>
-  );
-};
-
-var INLINE_STYLES = [
-  {label: 'Bold', style: 'BOLD'},
-  {label: 'Italic', style: 'ITALIC'},
-  {label: 'Underline', style: 'UNDERLINE'},
-];
-
-const InlineStyleControls = (props) => {
-  var currentStyle = props.editorState.getCurrentInlineStyle();
-  return (
-    <div className="RichEditor-controls">
-      {INLINE_STYLES.map(type =>
-        <StyleButton
-          key={type.label}
-          active={currentStyle.has(type.style)}
-          label={type.label}
-          onToggle={props.onToggle}
-          style={type.style}
-        />
-      )}
-    </div>
-  );
-};
-
-
-const FIELD_REGEX = /{{[\w\-]+}}/g;
-
-function fieldStrategy(contentBlock, callback, contentState) {
-  findWithRegex(FIELD_REGEX, contentBlock, callback);
-}
-
-function findWithRegex(regex, contentBlock, callback) {
-  const text = contentBlock.getText();
-  let matchArr, start;
-  while ((matchArr = regex.exec(text)) !== null) {
-    start = matchArr.index;
-    callback(start, start + matchArr[0].length);
-  }
-}
-
-const FieldSpan = (props) => {
-  return (
-    <span
-      style={styles.field}
-      data-offset-key={props.offsetKey}
-    >
-      {props.children}
-    </span>
-  );
-};
-
-const styles = {
-  field: {
-    color: '#000',
-    backgroundColor: '#f4f4ff',
-    border: '1px solid #eef',
-    padding: 2,
-    fontFamily: 'monospace',
-    fontSize: '90%',
-    fontWeight: 800,
-  },
-  fieldInputContainer: {
-    display: 'inline-block',
-  },
-  fieldInput: {},
-};
-
 export default TemplateEditor;
-
