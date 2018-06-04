a1abd5707da3a99b891005462be3bb66bd83095c
Luca Guindani <luca.guindani@gmail.com>
2018-06-04 15:56:30 +0200

    Added missing translations for catalog settings/item views/menu items/pages/users

--- app/helpers/catalog_admin/item_views_helper.rb
+++ app/helpers/catalog_admin/item_views_helper.rb
@@ -1,16 +1,16 @@
 module CatalogAdmin::ItemViewsHelper
   def list_view_badge(item_view)
     return unless item_view.default_for_list_view
-    content_tag(:span, "List view", :class => "label label-success")
+    content_tag(:span, I18n.t('item_list'), :class => "label label-success")
   end
 
   def item_view_badge(item_view)
     return unless item_view.default_for_item_view
-    content_tag(:span, "Item view", :class => "label label-warning")
+    content_tag(:span, I18n.t('item_view'), :class => "label label-warning")
   end
 
   def display_name_badge(item_view)
     return unless item_view.default_for_display_name
-    content_tag(:span, 'Display name', class: 'label label-primary')
+    content_tag(:span, I18n.t('item_display_name'), class: "label label-primary")
   end
 end
