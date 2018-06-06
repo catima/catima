af115af4d3548922023546b1c36f7050d6ce1e37
Luca Guindani <luca.guindani@gmail.com>
2018-06-06 14:24:43 +0200

    Added codeclimate to travis configuration & modified xref tests

--- test/models/field/xref_test.rb
+++ test/models/field/xref_test.rb
@@ -6,22 +6,20 @@ class Field::XrefTest < ActiveSupport::TestCase
   should validate_presence_of(:xref)
   should_not allow_value("not a URL").for(:xref)
 
-  unless ENV['TRAVIS']
-    test "checks that xref is a valid service" do
-      field = Field::Xref.new(
-        :slug => "xref",
-        :field_set => item_types(:one),
-        :name_en => "test",
-        :name_plural_en => "tests"
-      )
+  test "checks that xref is a valid service" do
+    field = Field::Xref.new(
+      :slug => "xref",
+      :field_set => item_types(:one),
+      :name_en => "test",
+      :name_plural_en => "tests"
+    )
 
-      with_expiring_vcr_cassette do
-        field.xref = "https://api.github.com/repos/rails/rails"
-        refute(field.valid?)
+    with_expiring_vcr_cassette do
+      field.xref = "https://api.github.com/repos/rails/rails"
+      refute(field.valid?)
 
-        field.xref = "https://catima-xref.herokuapp.com/api/v1"
-        assert(field.valid?)
-      end
+      field.xref = "https://catima-xref.herokuapp.com/api/v1"
+      assert(field.valid?)
     end
   end
 end
