bb26a5563d7d86092e0a6c198ec8d98b19369bbe
Luca Guindani <luca.guindani@gmail.com>
2018-06-07 12:02:53 +0200

    VCR modifications

--- test/support/vcr.rb
+++ test/support/vcr.rb
@@ -1,6 +1,6 @@
 
 VCR.configure do |config|
-  config.allow_http_connections_when_no_cassette = true
+  config.allow_http_connections_when_no_cassette = false
   config.cassette_library_dir = File.expand_path("../../cassettes", __FILE__)
   config.hook_into :webmock
   config.ignore_localhost = true
