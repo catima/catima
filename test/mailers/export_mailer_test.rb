8df0e1b2ca4b4d516c8a8afc4b5107dd42a8637a
Luca Guindani <luca.guindani@gmail.com>
2018-06-26 10:32:46 +0200

    Added export mailer test

--- test/mailers/export_mailer_test.rb
+++ test/mailers/export_mailer_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ExportMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "export" do
+    export = exports(:one)
+    mail = ExportMailer.send_message(export)
+
+    assert_equal("Catima - requested export is ready", mail.subject)
+    assert_equal(["no-reply@catima.unil.ch"], mail.from)
+    assert_equal(["one-admin@example.com"], mail.to)
+    assert_match("/one/en/admin/_exports/#{export.id}/download", mail.body.encoded)
+  end
 end
