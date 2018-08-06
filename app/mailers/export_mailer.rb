91b6229f6099e22b9a6963671cc553fbe6e57ce4
Luca Guindani <luca.guindani@gmail.com>
2018-08-06 11:10:12 +0200

    Fix export bug & change mail.from to MAIL_SENDER envar

--- app/mailers/export_mailer.rb
+++ app/mailers/export_mailer.rb
@@ -6,7 +6,7 @@ class ExportMailer < ApplicationMailer
     mail(
       :subject => t("export_mailer.export.subject"),
       :to => export.user.email,
-      :from => "no-reply@catima.unil.ch",
+      :from => [ENV['MAIL_SENDER']],
       :template_name => "export"
     )
   ensure
