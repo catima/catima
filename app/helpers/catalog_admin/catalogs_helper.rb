5e63a08d70fc8d570ca6514144775fad5fbde6f0
Luca Guindani <luca.guindani@gmail.com>
2018-10-02 10:51:29 +0200

    Translation and ui stuff (#77)
    * Translation for the groups feature
    
    * UI bugs & code optimization

--- app/helpers/catalog_admin/catalogs_helper.rb
+++ app/helpers/catalog_admin/catalogs_helper.rb
@@ -18,4 +18,20 @@ module CatalogAdmin::CatalogsHelper
     return 2 if catalog.visible && catalog.restricted
     3
   end
+
+  def catalog_access_select(catalog)
+    select_tag(
+      :catalog_access,
+      options_for_select(
+        [
+          [t('catalog_admin.catalogs.common_form_fields.open_for_everyone'), 1],
+          [t('catalog_admin.catalogs.common_form_fields.open_to_members'), 2],
+          [t('catalog_admin.catalogs.common_form_fields.open_to_catalog_staff'), 3]
+        ],
+        catalog_access(catalog)
+      ),
+      class: 'form-control',
+      label: 'catalog_access'
+    )
+  end
 end
--- test/integration/catalog_admin/catalogs_test.rb
+++ test/integration/catalog_admin/catalogs_test.rb
@@ -1,33 +1,4 @@
 require "test_helper"
 
 class CatalogAdmin::CatalogsTest < ActionDispatch::IntegrationTest
-  setup { use_javascript_capybara_driver }
-
-  test "set catalog visibility" do
-    log_in_as('two-admin@example.com', 'password')
-    visit('/two/en/admin/_settings')
-
-    catalog = Catalog.find_by(slug: 'two')
-    assert(catalog.visible?)
-    assert_not(catalog.restricted?)
-    assert_equal(find('#catalog_access').value, '1')
-
-    select('Open to members', from: 'catalog_access')
-    click_on('Save')
-    sleep 2
-
-    catalog = Catalog.find_by(slug: 'two')
-    assert(catalog.visible?)
-    assert(catalog.restricted?)
-
-    visit('/two/en/admin/_settings')
-    assert_equal(find('#catalog_access').value, '2')
-
-    select('Open to catalog staff', from: 'catalog_access')
-    click_on('Save')
-    sleep 2
-
-    catalog = Catalog.find_by(slug: 'two')
-    assert_not(catalog.visible?)
-  end
 end
