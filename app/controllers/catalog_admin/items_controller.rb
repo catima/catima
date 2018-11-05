3b45c9000b78af2a6e75cc944fecb8d349151aee
Grégoire Willmann <gregoire@kinoba.fr>
2018-11-05 11:55:07 +0100

    Add search function to catalog admin data pages (#95)
    * Add search function to catalog admin data pages
    
    * Fix code quality warning
    
    * Fix integration tests
    
    * Add placeholder for search field in admin data view
    
    * Add back to list link when editing an item in admin catalog for easier naviguation

--- app/controllers/catalog_admin/items_controller.rb
+++ app/controllers/catalog_admin/items_controller.rb
@@ -38,6 +38,8 @@ class CatalogAdmin::ItemsController < CatalogAdmin::BaseController
   end
 
   def edit
+    @back_to_list_path = request.referer
+
     find_item
     authorize(@item)
   end
@@ -84,6 +86,19 @@ class CatalogAdmin::ItemsController < CatalogAdmin::BaseController
       :catalog => params[:catalog_slug],
       :item_type => params[:item_type_slug], :field => fld_id
     }
+  end
+
+  def search
+    @search_results = ItemList::SimpleSearchResult.new(
+      :catalog => catalog,
+      :query => params[:q],
+      :page => params[:page],
+      :item_type_slug => params[:item_type_slug]
+    )
+    @items = apply_sort(policy_scope(@search_results.items))
+    @items = @items.page(params[:page]).per(25)
+    @fields = @item_type.all_list_view_fields
+    render("index", :layout => "catalog_admin/data")
   end
 
   private
---
+++ test/integration/catalog_admin/items_search_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class CatalogAdmin::ItemsSearchTest < ActionDispatch::IntegrationTest
+  setup { use_javascript_capybara_driver }
+
+  test "displays the items the admin is searching for" do
+    log_in_as("one-admin@example.com", "password")
+
+    visit("/one/en/admin/authors")
+    fill_in("q", :with => "Steve")
+    click_on('Search')
+
+    assert(page.has_content?('Stephen King'))
+  end
+
+  test "displays the items the admin is searching for in the right order" do
+    log_in_as("one-admin@example.com", "password")
+
+    visit("/one/en/admin/authors")
+    fill_in("q", :with => "old")
+    click_on("Search")
+
+    assert(page.has_content?("Very Old"))
+    assert(page.has_content?("Very Young"))
+
+    assert_equal("Very Old", find(:xpath, "//table/tbody/tr[1]/td[1]").text)
+    assert_equal("Very Young", find(:xpath, "//table/tbody/tr[2]/td[1]").text)
+  end
+end
