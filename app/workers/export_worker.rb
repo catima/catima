7175dc5489848a0272d20c6d541cb93acfb6eec7
Grégoire Willmann <gregoire@kinoba.fr>
2019-01-21 16:04:21 +0100

    Improvements for sql export

--- app/workers/export_worker.rb
+++ app/workers/export_worker.rb
@@ -23,12 +23,12 @@ class ExportWorker
 
   def catima_export(export, dir)
     status = "ready"
-    begin
+    # begin
       CatalogDump.new.dump(export.catalog.slug, dir)
       zip(dir, export.pathname)
-    rescue StandardError
-      status = "error"
-    end
+    # rescue StandardError
+    #   status = "error"
+    # end
     export.update(status: status)
     send_mail(export)
   end
