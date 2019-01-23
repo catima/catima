f69babc91fd155bd4ac0b228373aedb35360268e
Grégoire Willmann <gregoire@kinoba.fr>
2019-01-23 22:15:11 +0100

    Fix reference search

--- app/models/search/email_strategy.rb
+++ app/models/search/email_strategy.rb
@@ -7,11 +7,12 @@ class Search::EmailStrategy < Search::BaseStrategy
 
   def search(scope, criteria)
     negate = criteria[:field_condition] == "exclude"
-
+    p "000000000000000000000000000000000000000000000000000000000000"
+p scope.to_sql
     scope = exact_search(scope, criteria[:exact], negate)
     scope = all_words_search(scope, criteria[:all_words], negate)
     scope = one_word_search(scope, criteria[:one_word], negate)
-
+p scope.to_sql
     scope
   end
 
