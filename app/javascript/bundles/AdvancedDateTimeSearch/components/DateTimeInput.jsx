83554f6cdc9e20fc0701fe4cccde94d2dc4e674f
Ouriel Boiria <ouriel.boiria@gmail.com>
2019-01-24 10:17:32 +0100

    Fix DateTime hidden input name

--- app/javascript/bundles/AdvancedDateTimeSearch/components/DateTimeInput.jsx
+++ app/javascript/bundles/AdvancedDateTimeSearch/components/DateTimeInput.jsx
@@ -271,7 +271,7 @@ class DateTimeInput extends React.Component {
               ) : null
               }
               <div className="hidden-datepicker">
-                <input type="text" ref="hiddenInput" value={this.state.selectedDate} name={this.props.inputName} onChange={this.selectDate}/>
+                <input type="text" ref="hiddenInput" value={this.state.selectedDate} onChange={this.selectDate}/>
               </div>
               <div className="calendar-button-container">
                 <a id={this.props.inputId + '_calendar_icon' + '_' + this.props.inputSuffixId} onClick={this.openCloseDatepicker} type="button"><span className="glyphicon glyphicon-calendar"></span></a>
