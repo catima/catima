de10e87f1370c36f0a225a667e6f90f597e626f3
Grégoire Willmann <gregoire@kinoba.fr>
2019-01-25 13:21:08 +0100

    DateTime search functionnal + tests fix

--- app/models/search/date_time_strategy.rb
+++ app/models/search/date_time_strategy.rb
@@ -11,17 +11,17 @@ class Search::DateTimeStrategy < Search::BaseStrategy
     negate = criteria[:field_condition] == "exclude"
 
     start_condition = criteria[:start].keys.first
-    end_condition = criteria[:start].keys.first
+    end_condition = criteria[:end].keys.first
 
     # start_date_time = Time.zone.at(criteria[:start][start_condition].to_i / 1000) if start_date?(criteria)
     # end_date_time = Time.zone.at(criteria[:end][end_condition].to_i / 1000) if end_date?(criteria)
     start_date_time = criteria[:start][start_condition]
     end_date_time = criteria[:end][end_condition] if end_date?(criteria)
 
-    return scope unless start_date_time.present? || end_date_time.present?
+    return scope if components_empty?(start_date_time) && components_empty?(end_date_time)
 
     scope = append_where_date_is_set(scope)
-    scope = search_with_start_date(scope, criteria, start_date_time, negate, field_condition)
+    scope = search_with_start_date(scope, criteria, start_date_time, field_condition, negate)
     scope = search_interval_dates(
       scope, criteria, { :start => start_date_time, :end => end_date_time }, negate, field_condition
     )
@@ -50,10 +50,10 @@ class Search::DateTimeStrategy < Search::BaseStrategy
     criteria[:end].present? && criteria[:end][criteria[:end].keys.first].present?
   end
 
-  def search_with_start_date(scope, criteria, start_date_time, negate, field_condition)
+  def search_with_start_date(scope, criteria, start_date_time, field_condition, negate)
     return scope unless start_date?(criteria)
 
-    return exact_search(scope, start_date_time, negate) if field_condition == "exact"
+    return exact_search(scope, start_date_time, negate) if field_condition["exact"].present?
     return inexact_search(scope, start_date_time, field_condition, negate) if %w[before after].include?(field_condition)
 
     scope
@@ -67,16 +67,18 @@ class Search::DateTimeStrategy < Search::BaseStrategy
     interval_search(scope, dates[:start], dates[:end], field_condition, negate)
   end
 
-  def exact_search(scope, exact_date_time, negate)
-    return scope if exact_date_time.blank?
+  def exact_search(scope, date_time, negate)
+    return scope if date_time.blank?
 
     sql_operator = negate ? "<>" : "="
-    p "__________________________________________________________________________________"
-    p field.format
-    scope.where(
-      "#{convert_to_timestamp(concat_json_date(exact_date_time))} #{sql_operator}
-      to_timestamp(?, '#{field_date_format_to_sql_format}')", date_remove_utc(exact_date_time)
-    )
+
+    date_time.keys.each do |key|
+      next if date_time[key].blank?
+
+      scope = scope.where("#{data_field_expr(key)} #{sql_operator} ?", date_time[key])
+    end
+
+    scope
   end
 
   def inexact_search(scope, date_time, field_condition, negate)
@@ -96,11 +98,10 @@ class Search::DateTimeStrategy < Search::BaseStrategy
   end
 
   def interval_search(scope, start_date_time, end_date_time, field_condition, negate)
-    return scope if start_date_time.blank? || end_date_time.blank?
+    return inexact_search(scope, end_date_time, "before", field_condition == "outside") if start_date_time.blank?
+    return inexact_search(scope, start_date_time, "after", field_condition == "outside") if end_date_time.blank?
 
-    if negate
-      field_condition = field_condition == "between" ? "outside" : "between"
-    end
+    field_condition = field_condition == "between" ? "outside" : "between" if negate
 
     where_scope = ->(*q) { field_condition == "outside" ? scope.where.not(q) : scope.where(q) }
 
@@ -114,35 +115,35 @@ class Search::DateTimeStrategy < Search::BaseStrategy
   end
 
   # rubocop:disable Metrics/MethodLength
-  def concat_json_date(datetime)
+  def concat_json_date(date_time)
     "CONCAT(
       CASE WHEN items.data->'#{field.uuid}'->>'Y' IS NULL
-           THEN '#{datetime.strftime('%Y')}'
+           THEN '#{date_time_component(date_time, 'Y')}'
            ELSE LPAD(items.data->'#{field.uuid}'->>'Y', 4, '0')
            END,
         '-',
       CASE WHEN items.data->'#{field.uuid}'->>'M' IS NULL
-           THEN '#{datetime.strftime('%m')}'
+           THEN '#{date_time_component(date_time, 'M')}'
            ELSE LPAD(items.data->'#{field.uuid}'->>'M', 2, '0')
            END,
         '-',
       CASE WHEN items.data->'#{field.uuid}'->>'D' IS NULL
-           THEN '#{datetime.strftime('%d')}'
+           THEN '#{date_time_component(date_time, 'D')}'
            ELSE LPAD(items.data->'#{field.uuid}'->>'D', 2, '0')
            END,
         ' ',
       CASE WHEN items.data->'#{field.uuid}'->>'h' IS NULL
-           THEN '#{datetime.strftime('%H')}'
+           THEN '#{date_time_component(date_time, 'h')}'
            ELSE LPAD(items.data->'#{field.uuid}'->>'h', 2, '0')
            END,
         ':',
       CASE WHEN items.data->'#{field.uuid}'->>'m' IS NULL
-           THEN '#{datetime.strftime('%M')}'
+           THEN '#{date_time_component(date_time, 'm')}'
            ELSE LPAD(items.data->'#{field.uuid}'->>'m', 2, '0')
            END,
         ':',
       CASE WHEN items.data->'#{field.uuid}'->>'s' IS NULL
-           THEN '#{datetime.strftime('%S')}'
+           THEN '#{date_time_component(date_time, 's')}'
            ELSE LPAD(items.data->'#{field.uuid}'->>'s', 2, '0')
            END
     )"
@@ -158,10 +159,29 @@ class Search::DateTimeStrategy < Search::BaseStrategy
   end
 
   def date_remove_utc(date)
-    date.strftime("%Y-%m-%d %H:%M:%S")
+    utc_date = "#{date_time_component(date, 'Y')}-#{date_time_component(date, 'M')}-#{date_time_component(date, 'D')}"
+    utc_date << " #{date_time_component(date, 'h')}:#{date_time_component(date, 'm')}:#{date_time_component(date, 's')}"
+
+    # date.strftime("%Y-%m-%d %H:%M:%S")
   end
 
   def append_where_date_is_set(scope)
     scope.where("items.data->'#{field.uuid}' IS NOT NULL")
+  end
+
+  def data_field_expr(date_component)
+    "items.data->'#{field.uuid}'->>'#{date_component}'"
+  end
+
+  def date_time_component(date_time, key)
+    return date_time[key].presence || "0000" if key == "Y"
+
+    date_time[key].presence || "00"
+  end
+
+  def components_empty?(date_time)
+    date_time.each { |_key, value| return false if value.present? }
+
+    true
   end
 end
