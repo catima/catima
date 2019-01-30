ddb5815711fcde1b40eef1d276d5cef25c0f2d32
Ouriel Boiria <ouriel.boiria@gmail.com>
2019-01-30 20:33:56 +0100

    Prevent select to open when disabled for DateTimeInput

--- app/javascript/bundles/AdvancedDateTimeSearch/components/DateTimeInput.jsx
+++ app/javascript/bundles/AdvancedDateTimeSearch/components/DateTimeInput.jsx
@@ -45,6 +45,7 @@ class DateTimeInput extends React.Component {
 
     this.selectDate = this._selectDate.bind(this);
     this.openCloseDatepicker = this._openCloseDatepicker.bind(this);
+    this.onSelectMonthClick = this._onSelectMonthClick.bind(this);
     this.clearDatepicker = this._clearDatepicker.bind(this);
   }
 
