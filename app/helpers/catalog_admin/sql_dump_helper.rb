58952a618325784df0d70f500e9c9ca35fc4216c
Grégoire Willmann <gregoire@kinoba.fr>
2019-01-31 15:50:07 +0100

    Refactored code

--- app/helpers/catalog_admin/sql_dump_helper.rb
+++ app/helpers/catalog_admin/sql_dump_helper.rb
@@ -11,7 +11,34 @@ module CatalogAdmin::SqlDumpHelper
     "\n-- ••••••\n\n"
   end
 
+  def pretty_format_statement(statement)
+    remove_ending_comma!(statement)
+    add_return_carriages!(statement)
+  end
+
   def remove_ending_comma!(statement)
     statement.gsub!(/,$/, '')
+  end
+
+  def add_return_carriages!(statement)
+    statement.gsub!(/,/, ",\n")
+  end
+
+  def create_table(table_name, columns)
+    pretty_format_statement(columns)
+
+    "CREATE TABLE `#{table_name}` (\n#{columns}\n);\n\n"
+  end
+
+  def insert_into(table_name, columns, values)
+    "INSERT INTO `#{table_name}`\n\t\t(#{columns})\nVALUES (#{values});\n\n"
+  end
+
+  def add_primary_key(table_name, primary_key_name)
+    "ALTER TABLE `#{table_name}` ADD PRIMARY KEY (`#{primary_key_name}`);\n"
+  end
+
+  def add_foreign_key(table_name, fk_name, ref_table_name, ref_table_col)
+    "ALTER TABLE `#{table_name}` ADD FOREIGN KEY (`#{fk_name}`) REFERENCES `#{ref_table_name}`(`#{ref_table_col}`);\n\n"
   end
 end
