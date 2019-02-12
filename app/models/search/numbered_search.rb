7d31f88228f4529a7399621a7b7a2332ef86f221
Grégoire Willmann <gregoire@kinoba.fr>
2019-02-12 16:25:06 +0100

    Fix error with editor field and decimal field in search

--- app/models/search/numbered_search.rb
+++ app/models/search/numbered_search.rb
@@ -14,27 +14,31 @@ module Search::NumberedSearch
     return scope if exact_phrase.blank?
 
     sql_operator = negate ? ">=" : "<"
-    scope.where("(#{data_field_expr})::#{type} #{sql_operator} ?", exact_phrase.strip)
+    scope.where("(#{data_field_expr})::#{type} #{sql_operator} ?", exact_phrase.strip.to_f)
   end
 
   def less_than_or_equal_to_search(scope, exact_phrase, negate, type="int")
     return scope if exact_phrase.blank?
 
     sql_operator = negate ? ">" : "<="
-    scope.where("(#{data_field_expr})::#{type} #{sql_operator} ?", exact_phrase.strip)
+    scope.where("(#{data_field_expr})::#{type} #{sql_operator} ?", exact_phrase.strip.to_f)
   end
 
   def greater_than_search(scope, exact_phrase, negate, type="int")
     return scope if exact_phrase.blank?
 
     sql_operator = negate ? "<=" : ">"
-    scope.where("(#{data_field_expr})::#{type} #{sql_operator} ?", exact_phrase.strip)
+    scope.where("(#{data_field_expr})::#{type} #{sql_operator} ?", exact_phrase.strip.to_f)
   end
 
   def greater_than_or_equal_to_search(scope, exact_phrase, negate, type="int")
     return scope if exact_phrase.blank?
 
     sql_operator = negate ? "<" : ">="
-    scope.where("(#{data_field_expr})::#{type} #{sql_operator} ?", exact_phrase.strip)
+    scope.where("(#{data_field_expr})::#{type} #{sql_operator} ?", exact_phrase.strip.to_f)
+  end
+
+  def append_where_number_is_set(scope)
+    scope.where("#{data_field_expr} <> ''")
   end
 end
