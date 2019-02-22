a7c079adedc24b3a07191be0e1abaf468bd0825f
Ouriel Boiria <ouriel.boiria@gmail.com>
2019-02-22 16:35:20 +0100

    Remove unused model file

--- db/structure.sql
+++ db/structure.sql
@@ -9,20 +9,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
---
 -- Name: bigdate_to_num(json); Type: FUNCTION; Schema: public; Owner: -
 --
 
@@ -1936,6 +1922,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20181207103025'),
 ('20181207145518'),
 ('20181210123619'),
-('20181214095728');
+('20181214095728'),
+('20190201141740');
 
 
