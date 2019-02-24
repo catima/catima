b8a425660eb0d966f3bb5f95694b2cdfc257d1b2
Grégoire Willmann <gregoire@kinoba.fr>
2019-02-24 23:11:57 +0100

    Order search results by primary key

--- test/integration/advanced_searches_test.rb
+++ test/integration/advanced_searches_test.rb
@@ -25,4 +25,26 @@ class AdvancedSearchesTest < ActionDispatch::IntegrationTest
     click_on("Highlander")
     within("h1") { assert(page.has_content?("Highlander")) }
   end
+
+  test "displays the results in the alphabetical order" do
+    visit("/search/en")
+    click_on("Advanced")
+
+    find("#default_search_type").click
+    within("#default_search_type") do
+      click_on("Vehicle")
+    end
+
+    fill_in(
+      "advanced_search[criteria][search_vehicle_make_uuid][exact]",
+      :with => "toyota"
+    )
+    click_on("Search")
+
+    items = all("h4")
+
+    assert_equal("Camry", items[0].text)
+    assert_equal("Camry Hybrid", items[1].text)
+    assert_equal("Highlander", items[2].text)
+  end
 end
--- test/integration/simple_search_test.rb
+++ test/integration/simple_search_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class SimpleSearchTest < ActionDispatch::IntegrationTest
+  setup { use_javascript_capybara_driver }
+
   setup do
     catalogs(:search).items.reindex
   end
@@ -13,6 +15,18 @@ class SimpleSearchTest < ActionDispatch::IntegrationTest
     assert(page.has_content?("Prius"))
     assert(page.has_content?("Highlander"))
     assert(page.has_content?("Camry"))
+  end
+
+  test "displays the results in the alphabetical order" do
+    visit("/search/en")
+    fill_in("q", :with => "Toyota")
+    click_on("Search")
+
+    items = all("h4")
+
+    assert_equal("Camry", items[0].text)
+    assert_equal("Camry Hybrid", items[1].text)
+    assert_equal("Highlander", items[2].text)
   end
 
   test "search for honda finds no matches" do
