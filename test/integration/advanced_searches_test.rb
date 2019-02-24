a427632c831f43d50a5e425a6579a0848e29712e
Grégoire Willmann <gregoire@kinoba.fr>
2019-02-24 23:11:57 +0100

    Order search results by primary key

--- test/integration/advanced_searches_test.rb
+++ test/integration/advanced_searches_test.rb
@@ -24,4 +24,26 @@ class AdvancedSearchesTest < ActionDispatch::IntegrationTest
     assert_equal("Camry Hybrid", items[1].text)
     assert_equal("Highlander", items[2].text)
   end
+
+  test "displays the results in the alphabetical order" do
+    visit("/search/en")
+    click_on("Advanced")
+
+    find("#default_search_type").click
+    within("#default_search_type") do
+      click_on("Vehicle")
+    end
+
+    fill_in(
+      "advanced_search[criteria][search_vehicle_make_uuid][exact]",
+      :with => "toyota"
+    )
+    click_on("Search")
+
+    items = all("h4")
+
+    assert_equal("Camry", items[0].text)
+    assert_equal("Camry Hybrid", items[1].text)
+    assert_equal("Highlander", items[2].text)
+  end
 end
