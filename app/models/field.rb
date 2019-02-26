9ae93baed9ffbc014e14e1f720eab501c6714bea
Grégoire Willmann <gregoire@kinoba.fr>
2019-02-26 17:25:24 +0100

    Add multiple fields to sql export

--- app/models/field.rb
+++ app/models/field.rb
@@ -336,7 +336,7 @@ class Field < ApplicationRecord
   def sql_default
     return "" if default_value.blank?
 
-    "DEFAULT #{default_value}"
+    "DEFAULT '#{default_value}'"
   end
 
   private
--- app/models/field/file.rb
+++ app/models/field/file.rb
@@ -61,6 +61,6 @@ class Field::File < ::Field
   end
 
   def sql_type
-    "VARCHAR(255)"
+    "TEXT"
   end
 end
