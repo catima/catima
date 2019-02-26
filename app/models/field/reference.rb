5c98651a8ad3576b05d859aeda6485c3b1910a38
Grégoire Willmann <gregoire@kinoba.fr>
2019-02-26 16:16:10 +0100

    All primary keys are now the id of the table

--- app/models/field/reference.rb
+++ app/models/field/reference.rb
@@ -135,9 +135,10 @@ class Field::Reference < ::Field
   end
 
   def sql_type
-    return "INT" if related_item_type.primary_field.blank?
-
-    related_item_type.primary_field.sql_type
+    "INT"
+    # return "INT" if related_item_type.primary_field.blank?
+    #
+    # related_item_type.primary_field.sql_type
     # while related_primary_field.is_a?(Field::Reference) && related_primary_field.related_item_type.present?
     #   related_primary_field = related_item_type.primary_field.sql_type
     # end
