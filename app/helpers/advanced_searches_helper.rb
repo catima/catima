0cf999be2eae83546821c091e6b9cb1066340f1b
Grégoire Willmann <gregoire@kinoba.fr>
2019-04-02 15:33:04 +0200

    Fix other linter warnings

--- app/helpers/advanced_searches_helper.rb
+++ app/helpers/advanced_searches_helper.rb
@@ -29,38 +29,15 @@ module AdvancedSearchesHelper
     partial_rendered = ""
     content = form.fields_for(:criteria) do |f|
       f.fields_for(field.uuid) do |f|
-        partial_rendered = render(partial, :f => f, :field => field, :i => index)
+        begin
+          partial_rendered = render(partial, :f => f, :field => field, :i => index)
+        rescue ActionView::MissingTemplate
+          partial_rendered = nil
+        end
       end
     end
     result = capture(content, &block)
     partial_rendered.present? ? result : partial_rendered
-  end
-
-  def render_item_types_as_options(item_types, selected_item_type_slug, configuration)
-    options = []
-    item_types.each do |item_type|
-      options << [
-        item_type.name,
-        item_type.slug,
-        :selected => item_type.slug == selected_item_type_slug,
-        "data-has-map" => item_type.include_geographic_field?
-      ]
-    end
-
-    options
-  end
-
-  def render_and_or_exclude_select(form)
-    form.select(
-      "field_condition",
-      [
-        [I18n.t(".and"), "and"],
-        [I18n.t(".or"), "or"],
-        [I18n.t(".exclude"), "exclude"]
-      ],
-      { :hide_label => true },
-      :class => "field-condition"
-    )
   end
 
   def render_item_types_as_options(item_types, selected_item_type_slug)
