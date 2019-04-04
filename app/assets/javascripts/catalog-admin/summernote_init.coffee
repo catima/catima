d432722abf3c6956ebbe4349bb889ec4c8509971
Luca Guindani <luca.guindani@gmail.com>
2019-04-04 14:55:09 +0200

    Add HTML containers image size limit Fix HTML containers image bug

--- app/assets/javascripts/catalog-admin/summernote_init.coffee
+++ app/assets/javascripts/catalog-admin/summernote_init.coffee
@@ -1,5 +1,6 @@
 init = ->
   $("[data-provider='summernote']").summernote
     height: 300
+    maximumImageFileSize: 262144 # 256KB
 
 $(document).ready(init)
--- app/helpers/summernote_helper.rb
+++ app/helpers/summernote_helper.rb
@@ -4,20 +4,8 @@ module SummernoteHelper
     form.text_area(
       method,
       :rows => 16,
-      :value => summernote_safe_value(value),
+      :value => value,
       :data => { :provider => "summernote" }
     )
-  end
-
-  # Safari has a hard time rendering base64 images if the base64 data does not
-  # contain line breaks (it will peg the CPU for seconds or minutes). The
-  # workaround is to split the base64 into short lines. This method detects
-  # base64 image data inside an HTML value and modifies it by inserting line
-  # breaks. Returns a modified copy of the original value.
-  def summernote_safe_value(value)
-    value.gsub(%r{"data:image/jpeg;base64,([^"]*)"}) do
-      base64_data = $1.scan(/.{1,76}/).join("\n")
-      %Q("data:image/jpeg;base64,#{base64_data}")
-    end
   end
 end
