17635fb2dfcd86f333251fe036ebb60ef0112e23
Luca Guindani <luca.guindani@gmail.com>
2019-05-01 14:45:36 +0200

    Disabled restricted option if field is primary

--- app/helpers/catalog_admin/fields_option_helper.rb
+++ app/helpers/catalog_admin/fields_option_helper.rb
@@ -4,12 +4,20 @@ module CatalogAdmin::FieldsOptionHelper
 
     return false unless field.human_readable?
 
+    return false if field.restricted?
+
     true
   end
 
   def formatted_option?(field)
     return false unless field.respond_to?(:formatted?)
 
+    return false if field.primary?
+
+    true
+  end
+
+  def restricted_option?(field)
     return false if field.primary?
 
     true
