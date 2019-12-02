28116cad75542f38e3ded8759ead630fb4aab4f3
Luca Guindani <luca.guindani@gmail.com>
2019-12-02 16:15:48 +0100

    Fix CI (#256)
    * Fix CI
    * Update sidekiq configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,7 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://localhost:6379/1" }
+  config.redis = { url: "redis://" + ENV.fetch("REDIS_HOST", "localhost") + ":6379/1" }
   config.redis = { db: 1 }
 end
 
