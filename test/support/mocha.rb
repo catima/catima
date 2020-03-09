1f0311bc48c74fe902328a332a67181a2bf92b01
Luca Guindani <luca.guindani@gmail.com>
2020-03-09 14:15:17 +0100

    Update configurations after ruby dependencies upgrade

--- test/support/mocha.rb
+++ test/support/mocha.rb
@@ -1,4 +1,4 @@
 # Mocha provides mocking and stubbing helpers
 require "mocha/minitest"
-Mocha::Configuration.warn_when(:stubbing_non_existent_method)
-Mocha::Configuration.warn_when(:stubbing_non_public_method)
+Mocha.configure { |c| c.stubbing_non_existent_method = :warn }
+Mocha.configure { |c| c.stubbing_non_public_method = :warn }
