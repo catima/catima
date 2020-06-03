481aa1dbee2afc487cc58c7696ef56927a3446c9
Luca Guindani <luca.guindani@gmail.com>
2020-06-03 08:22:58 +0200

    Remove liquid handler from template storage

--- test/integration/template_test.rb
+++ test/integration/template_test.rb
@@ -14,18 +14,4 @@ class DatabaseTemplateTest < ActionDispatch::IntegrationTest
     tpl.path = 'home/index2'
     tpl.save!
   end
-
-  test "shows custom liquid template for home page" do
-    config = Configuration.first!
-    config.update!(:root_mode => "custom")
-
-    tpl = template_storages(:liquid_template)
-    tpl.path = 'home/index'
-    tpl.save!
-
-    visit("/")
-    assert(page.has_content?(/Hello WORLD has 5 letters!/i))
-    tpl.path = 'home/index3'
-    tpl.save!
-  end
 end
