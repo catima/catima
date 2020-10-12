d8600c3a2864938fa5479bcf764677842d1802a9
Luca Guindani <luca.guindani@gmail.com>
2020-10-12 14:22:56 +0200

    Update Bugsnag initializer

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,7 +1,7 @@
 Bugsnag.configure do |config|
   config.api_key = ENV["BUGSNAG_API_KEY"]
 
-  config.notify_release_stages = ['production']
+  config.release_stage = ENV["RAILS_ENV"]
 
-  config.release_stage = ENV['CLUE_OVERRIDE'] || ENV["RAILS_ENV"]
+  config.notify_release_stages = %w[staging production]
 end
