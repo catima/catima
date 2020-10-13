2c9829309f8a03da36c34cba8efe382b40661557
Luca Guindani <luca.guindani@gmail.com>
2020-10-13 15:30:52 +0200

    Add app version to bugsnag configuration

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,6 +1,8 @@
 Bugsnag.configure do |config|
   config.api_key = ENV["BUGSNAG_API_KEY"]
 
+  config.app_version = ENV["APP_VERSION"]
+
   config.release_stage = ENV["RAILS_ENV"]
 
   config.notify_release_stages = %w[staging production]
