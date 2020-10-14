995fecd2503823dcba66e3b20f713258a415cf7b
Luca Guindani <luca.guindani@gmail.com>
2020-10-14 08:49:31 +0200

    Update app version format

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,7 +1,7 @@
 Bugsnag.configure do |config|
   config.api_key = ENV["BUGSNAG_API_KEY"]
 
-  config.app_version = ENV["APP_VERSION"]
+  config.app_version = ENV["APP_VERSION"] ? "v".concat(ENV["APP_VERSION"]) : 'undefined'
 
   config.release_stage = ENV["RAILS_ENV"]
 
