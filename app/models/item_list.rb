bd5716e2b2778efd918d0824a7aca6c7c33e5a6c
Manuel Wegria <abstract.s33d@gmail.com>
2021-10-27 11:07:16 +0200

    381 Advanced Search By Reference

--- app/models/item_list.rb
+++ app/models/item_list.rb
@@ -42,18 +42,23 @@ class ItemList
 
   def items
     @items ||= begin
-                 unpaginated_list_items = unpaginaged_items.includes(:item_type)
-                                                           .includes(:item_type => :fields)
-                                                           .page(page)
-                                                           .per(per)
-
-                 item_type ||= unpaginated_list_items.first&.item_type
                  return unpaginated_list_items if item_type.nil?
 
                  item_type ||= (catalog.presence || @selected_catalog).item_types.first
                  return unpaginated_list_items if item_type.primary_human_readable_field.nil?
 
                  unpaginated_list_items.order(Arel.sql("items.data->>'#{item_type.primary_human_readable_field.uuid}'"))
-               end
+               end.load
+  end
+
+  def unpaginated_list_items
+    @unpaginated_list_items ||= unpaginaged_items.includes(:item_type)
+                                                 .includes(:item_type => :fields)
+                                                 .page(page)
+                                                 .per(per)
+  end
+
+  def item_type
+    @item_type ||= unpaginated_list_items.first&.item_type
   end
 end
