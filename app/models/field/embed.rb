a10b1a6505d2b2910f35f9a954826f0bc0480b58
Manuel Wegria <abstract.s33d@gmail.com>
2021-11-10 14:43:15 +0100

    CR

--- app/models/field/embed.rb
+++ app/models/field/embed.rb
@@ -42,7 +42,7 @@ class Field::Embed < ::Field
   end
 
   def remove_width_height_value
-    update!(:options => options.except("width", "height")) if (options["widht"] || options["height"])
+    update!(:options => options.except("width", "height")) if (widht || height)
   end
 
   def build_validators
