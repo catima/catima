567350453a89cc66451bf274edd96d6237c9a186
Luca Guindani <luca.guindani@gmail.com>
2021-11-24 15:42:48 +0100

    Fix empty url embed field

--- app/presenters/field/embed_presenter.rb
+++ app/presenters/field/embed_presenter.rb
@@ -2,7 +2,7 @@ class Field::EmbedPresenter < FieldPresenter
   COMPACT_WIDTH = 300
   COMPACT_HEIGHT = 150
 
-  def input(form, method, options = {})
+  def input(form, method, options={})
     if field.iframe?
       form.text_area(method, input_defaults(options))
     elsif field.url?
@@ -11,10 +11,12 @@ class Field::EmbedPresenter < FieldPresenter
   end
 
   def value
+    return nil if raw_value.blank?
+
     if field.iframe?
       raw_value&.html_safe
       compact? ? raw_value&.gsub(/width=["']\d+["']/, "width=\"#{COMPACT_WIDTH}\"")&.gsub(/height=["']\d+["']/, "height=\"#{COMPACT_HEIGHT}\"")&.html_safe : raw_value&.html_safe
-    elsif field.url? && raw_value
+    elsif field.url?
       "<iframe width='#{compact? ? COMPACT_WIDTH : field.iframe_width}' height='#{compact? ? COMPACT_HEIGHT : field.iframe_height}' style='border: none;' src='#{raw_value}'></iframe>".html_safe
     end
   end
