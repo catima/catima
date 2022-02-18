4c9351990744c2f8469ee9559da00dc8a5e70243
Manuel Wegria <abstract.s33d@gmail.com>
2022-02-18 15:46:19 +0100

    revert locales changes

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -7,7 +7,7 @@ Locales.configure do |config|
   config.i18n_dir = Rails.root.join("app", "assets", "i18n")
 
   # Path to the rails locale files.
-  config.i18n_yml_dir = Rails.root.join("config", "locales")
+  config.i18n_yml_dir = Rails.root.join("config", "locales", "app")
 
   # i18n format
   config.i18n_output_format = 'js'
