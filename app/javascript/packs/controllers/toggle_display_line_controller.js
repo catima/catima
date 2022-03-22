c281d161de3c4de9f2069fbb1d160b17b9358bdd
Manuel Wegria <abstract.s33d@gmail.com>
2022-03-22 17:10:55 +0100

    fix

--- app/javascript/packs/controllers/toggle_display_line_controller.js
+++ app/javascript/packs/controllers/toggle_display_line_controller.js
@@ -1,10 +1,9 @@
 import {Controller} from "stimulus"
 
 export default class extends Controller {
-  static targets = ["revealable", "source", "upArrow", "downArrow"]
+  static targets = ["revealable", "upArrow", "downArrow"]
 
   connect() {
-    this.show = true
     this.reveal()
     this.revealableTargets.forEach(function (el, _) {
       el.classList.add("toggle-display")
@@ -12,65 +11,32 @@ export default class extends Controller {
   }
 
   reveal(e) {
-    let target = e?.target || (this.hasSourceTarget ? this.sourceTarget : false)
-    if (target) {
-      switch (target.nodeName) {
-        case "SELECT":
-          this.show = target.options[target.selectedIndex].value === this.data.get("value");
-          break;
-        case "INPUT":
-          switch (target.type) {
-            case "radio":
-              targets.forEach((el) => {
-                if (el.checked) {
-                  this.show = el.value;
-                }
-              });
-              break;
-            case "checkbox":
-              this.show = target.checked ? target.id : "";
-              break;
-          }
-      }
-    }
-
-    if (this.show) {
+    if (this.hasRevealableTarget && !this.revealableTarget.classList.contains('is-visible')) {
       if (this.hasRevealableTarget) {
-        this.showElement(this.revealableTarget)
+        this.revealableTarget.classList.add('is-visible')
+        this.revealableTarget.classList.remove('d-none')
+        if (this.hasUpArrowTarget) {
+          this.upArrowTarget.classList.add('d-none')
+        }
+        if (this.hasDownArrowTarget) {
+          this.downArrowTarget.classList.remove('d-none')
+        }
       }
     } else {
       if (this.hasRevealableTarget) {
-        this.hideElement(this.revealableTarget)
+        this.revealableTarget.classList.remove('is-visible')
+        if (this.hasUpArrowTarget) {
+          this.upArrowTarget.classList.remove('d-none')
+        }
+        if (this.hasDownArrowTarget) {
+          this.downArrowTarget.classList.add('d-none')
+        }
       }
-    }
-  }
-
-  showElement(el) {
-    this.show = !this.show
-    el.classList.add('is-visible')
-    el.classList.remove('d-none')
-    if (this.hasUpArrowTarget) {
-      this.upArrowTarget.classList.add('d-none')
-    }
-    if (this.hasDownArrowTarget) {
-      this.downArrowTarget.classList.remove('d-none')
-    }
-  }
-
-  hideElement(el) {
-    this.show = !this.show
-    el.classList.remove('is-visible')
-    if (this.hasUpArrowTarget) {
-      this.upArrowTarget.classList.remove('d-none')
-    }
-    if (this.hasDownArrowTarget) {
-      this.downArrowTarget.classList.add('d-none')
     }
   }
 
   showAllElements(e) {
     e.preventDefault()
-    this.show = !this.show
     document.querySelectorAll('[data-toggle-display-line-target="revealable"]').forEach(target => target.classList.add('is-visible'))
     document.querySelectorAll('[data-toggle-display-line-target="revealable"]').forEach(target => target.classList.remove('d-none'))
     document.querySelectorAll('[data-toggle-display-line-target="upArrow"]').forEach(target => target.classList.add('d-none'))
@@ -79,7 +45,6 @@ export default class extends Controller {
 
   hideAllElements(e) {
     e.preventDefault()
-    this.show = !this.show
     document.querySelectorAll('[data-toggle-display-line-target="revealable"]').forEach(target => target.classList.remove('is-visible'))
     document.querySelectorAll('[data-toggle-display-line-target="upArrow"]').forEach(target => target.classList.remove('d-none'))
     document.querySelectorAll('[data-toggle-display-line-target="downArrow"]').forEach(target => target.classList.add('d-none'))
