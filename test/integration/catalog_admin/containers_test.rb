d72d823806d80ab078669fde5deee41bf529c27e
Luca Guindani <luca.guindani@gmail.com>
2022-04-25 16:35:44 +0200

    Add tests for container line style & sorting

--- test/integration/catalog_admin/containers_test.rb
+++ test/integration/catalog_admin/containers_test.rb
@@ -70,4 +70,16 @@ class CatalogAdmin::ContainersTest < ActionDispatch::IntegrationTest
       assert(page.has_content?("Stephen"))
     end
   end
+
+  test "cannot create two itemlist containers in the same page" do
+    log_in_as("one-admin@example.com", "password")
+    visit("/one/en/admin/_pages/one_fdesc/edit")
+
+    find("#add-field-dropdown").click
+    click_on("ItemList")
+
+    click_on("Create container")
+
+    assert(page.has_content?('Multiple "ItemList" containers in the same page is not allowed'))
+  end
 end
