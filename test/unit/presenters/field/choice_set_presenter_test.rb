034cb147cac8ec42e3cefa4996fed1d6725dd493
David Miserez <david.miserez@unil.ch>
2023-01-10 17:04:54 +0100

    Fix choices presenter (#489)
    * Fix the character '>' to be displayed by error for hierarchical choices
    
    * Rubocop and reformat for better lisibility
    
    * Fix choice set presenter test
    
    * Add test for hierarchy choice set presenter
    
    * Rubocop and reformat for better lisibility
    
    * Fix broken choice set test
    
    * Add blank line after guard clause
    
    * Remove empty line

--- test/unit/presenters/field/choice_set_presenter_test.rb
+++ test/unit/presenters/field/choice_set_presenter_test.rb
@@ -10,16 +10,58 @@ class Field::ChoiceSetPresenterTest < ActionView::TestCase
     # Have to set this manually because fixture doesn't know ID ahead of time
     author.data["one_author_language_uuid"] = english.id
 
+    expected_html = <<~HTML
+      <div>
+        <div data-controller=\"hierarchy-revealable\">
+          <div data-hierarchy-revealable-target=\"choice\">
+            <a href=\"/one/en/authors?language=en-Eng\">English</a>
+          </div>
+          <div data-hierarchy-revealable-target=\"choice\" style=\"display: none\">
+            <a href=\"/one/en/authors?language=en-Eng\">English</a>
+            <span class=\"pl-2\" data-toggle=\"tooltip\" title=\"Hide hierarchy\" data-action=\"click-&gt;hierarchy-revealable#toggle\">
+              <i class=\"fa fa-caret-left toggle-hierarchy\"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    HTML
+
     presenter = Field::ChoiceSetPresenter.new(self, author, language_field)
     assert_equal(
-      "<div>"\
-          "<div data-controller=\"hierarchy-revealable\">"\
-          "<div data-hierarchy-revealable-target=\"choice\">"\
-          "<a href=\"/one/en/authors?language=en-Eng\">English</a><span class=\"pl-2\" data-toggle=\"tooltip\" title=\"Show hierarchy\" data-action=\"click-&gt;hierarchy-revealable#toggle\"><i class=\"fa fa-caret-right toggle-hierarchy\"></i></span></div><div data-hierarchy-revealable-target=\"choice\" style=\"display: none\">"\
-          "<a href=\"/one/en/authors?language=en-Eng\">English </a><span class=\"pl-2\" data-toggle=\"tooltip\" title=\"Hide hierarchy\" data-action=\"click-&gt;hierarchy-revealable#toggle\"><i class=\"fa fa-caret-left toggle-hierarchy\"></i></span>"\
-          "</div>"\
-          "</div>"\
-          "</div>",
+      expected_html.gsub(/(\n\s*)+/, ''), # Remove LF and indentation.
+      presenter.value
+    )
+  end
+
+  test "#value with hierarchy" do
+    author = items(:one_author_stephen_king)
+    english_uk = choices(:one_english_uk)
+    language_field = Field.find ActiveRecord::FixtureSet.identify('one_author_language')
+    # Have to set this manually because fixture doesn't know ID ahead of time
+    author.data["one_author_language_uuid"] = english_uk.id
+
+    expected_html = <<~HTML
+      <div>
+        <div data-controller=\"hierarchy-revealable\">
+          <div data-hierarchy-revealable-target=\"choice\">
+            <a href=\"/one/en/authors?language=en-Eng+%28UK%29\">English (UK)</a>
+            <span class=\"pl-2\" data-toggle=\"tooltip\" title=\"Show hierarchy\" data-action=\"click-&gt;hierarchy-revealable#toggle\">
+              <i class=\"fa fa-caret-right toggle-hierarchy\"></i>
+            </span>
+          </div>
+          <div data-hierarchy-revealable-target=\"choice\" style=\"display: none\">
+            <a href=\"/one/en/authors?language=en-Eng+%28UK%29\">English / English (UK)</a>
+            <span class=\"pl-2\" data-toggle=\"tooltip\" title=\"Hide hierarchy\" data-action=\"click-&gt;hierarchy-revealable#toggle\">
+              <i class=\"fa fa-caret-left toggle-hierarchy\"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    HTML
+
+    presenter = Field::ChoiceSetPresenter.new(self, author, language_field)
+    assert_equal(
+      expected_html.gsub(/(\n\s*)+/, ''), # Remove LF and indentation.
       presenter.value
     )
   end
@@ -31,18 +73,38 @@ class Field::ChoiceSetPresenterTest < ActionView::TestCase
     # Have to set this manually because fixture doesn't know IDs ahead of time
     author.data["one_author_other_language_uuid"] = choices.map(&:id)
 
+    # rubocop:disable Layout/TrailingWhitespace -> trailing whitespaces present in html.
+    expected_html = <<~HTML
+      <div>
+        <div data-controller=\"hierarchy-revealable\">
+          <div data-hierarchy-revealable-target=\"choice\">
+            <a href=\"/one/en/authors?other-language=en-Eng\">English</a>
+          </div>
+          <div data-hierarchy-revealable-target=\"choice\" style=\"display: none\">
+            <a href=\"/one/en/authors?other-language=en-Eng\">English</a>
+            <span class=\"pl-2\" data-toggle=\"tooltip\" title=\"Hide hierarchy\" data-action=\"click-&gt;hierarchy-revealable#toggle\">
+              <i class=\"fa fa-caret-left toggle-hierarchy\"></i>
+            </span>
+          </div>
+        </div> 
+        <div data-controller=\"hierarchy-revealable\">
+          <div data-hierarchy-revealable-target=\"choice\">
+            <a href=\"/one/en/authors?other-language=en-Spanish\">Spanish</a>
+          </div>
+          <div data-hierarchy-revealable-target=\"choice\" style=\"display: none\">
+            <a href=\"/one/en/authors?other-language=en-Spanish\">Spanish</a>
+            <span class=\"pl-2\" data-toggle=\"tooltip\" title=\"Hide hierarchy\" data-action=\"click-&gt;hierarchy-revealable#toggle\">
+              <i class=\"fa fa-caret-left toggle-hierarchy\"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    HTML
+    # rubocop:enable Layout/TrailingWhitespace
+
     presenter = Field::ChoiceSetPresenter.new(self, author, languages_field)
     assert_equal(
-      "<div>"\
-          "<div data-controller=\"hierarchy-revealable\">"\
-          "<div data-hierarchy-revealable-target=\"choice\"><a href=\"/one/en/authors?other-language=en-Eng\">English</a><span class=\"pl-2\" data-toggle=\"tooltip\" title=\"Show hierarchy\" data-action=\"click-&gt;hierarchy-revealable#toggle\"><i class=\"fa fa-caret-right toggle-hierarchy\"></i></span></div>"\
-          "<div data-hierarchy-revealable-target=\"choice\" style=\"display: none\"><a href=\"/one/en/authors?other-language=en-Eng\">English </a><span class=\"pl-2\" data-toggle=\"tooltip\" title=\"Hide hierarchy\" data-action=\"click-&gt;hierarchy-revealable#toggle\"><i class=\"fa fa-caret-left toggle-hierarchy\"></i></span></div>"\
-          "</div> "\
-          "<div data-controller=\"hierarchy-revealable\">"\
-          "<div data-hierarchy-revealable-target=\"choice\"><a href=\"/one/en/authors?other-language=en-Spanish\">Spanish</a><span class=\"pl-2\" data-toggle=\"tooltip\" title=\"Show hierarchy\" data-action=\"click-&gt;hierarchy-revealable#toggle\"><i class=\"fa fa-caret-right toggle-hierarchy\"></i></span></div>"\
-          "<div data-hierarchy-revealable-target=\"choice\" style=\"display: none\"><a href=\"/one/en/authors?other-language=en-Spanish\">Spanish </a><span class=\"pl-2\" data-toggle=\"tooltip\" title=\"Hide hierarchy\" data-action=\"click-&gt;hierarchy-revealable#toggle\"><i class=\"fa fa-caret-left toggle-hierarchy\"></i></span></div>"\
-          "</div>"\
-          "</div>",
+      expected_html.gsub(/(\n\s*)+/, ''), # Remove LF and indentation.
       presenter.value
     )
   end
