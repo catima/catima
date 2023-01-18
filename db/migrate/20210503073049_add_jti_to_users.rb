004fbc4f8b2d0af4c985728cf227b073b95a8de1
David Miserez <david.miserez@unil.ch>
2023-01-18 14:01:37 +0100

    Unscope user for jti migration

--- db/migrate/20210503073049_add_jti_to_users.rb
+++ db/migrate/20210503073049_add_jti_to_users.rb
@@ -1,8 +1,10 @@
 class AddJtiToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :jti, :string
-    User.all.each do |user|
+    User.with_deleted.all.each do |user|
+      # rubocop:disable Rails/SkipsModelValidations
       user.update_column(:jti, SecureRandom.uuid)
+      # rubocop:enable Rails/SkipsModelValidations
     end
     change_column_null :users, :jti, false
     add_index :users, :jti, unique: true
