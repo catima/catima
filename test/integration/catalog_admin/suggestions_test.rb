48d533b7319c3ed22c6216f185d8d0f3effc354a
Luca Guindani <luca.guindani@gmail.com>
2023-03-09 14:52:24 +0100

    Fix suggestions javascript issue in catalog admin Refactor suggestions tests

--- test/integration/catalog_admin/suggestions_test.rb
+++ test/integration/catalog_admin/suggestions_test.rb
@@ -27,4 +27,19 @@ class CatalogAdmin::SuggestionsTest < ActionDispatch::IntegrationTest
     refute(page.has_content?("Suggestions"))
     refute(page.has_content?(/existing_suggestion1/i))
   end
+
+  test "deleted user is still correctly displayed" do
+    log_in_as("one-admin@example.com", "password")
+
+    item = items(:one_author_stephen_king)
+    visit("one/en/admin/authors/#{item.to_param}/edit")
+
+    within(".suggestion-content") do
+      assert(page.has_content?(suggestions(:one_comment1).content))
+      first("a.toggle-suggestion").click
+
+      # Deleted user should still be displayed in suggestions.
+      assert(page.has_content?(users(:one_user_deleted).email))
+    end
+  end
 end
