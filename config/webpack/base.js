b06caa442abbc3a35a3704edd93d9ad9226f6a87
Luca Guindani <luca.guindani@gmail.com>
2023-08-03 15:20:00 +0200

    Upgrade webpacker from v5 to v6RC Upgrade most js dependencies

---
+++ config/webpack/base.js
@@ -0,0 +1,15 @@
+const { webpackConfig, merge } = require('@rails/webpacker')
+
+const options = {
+  resolve: {
+    extensions: [ '.coffee', '.erb', '.jsx', '.mjs', '.js', '.sass', '.scss', '.css', '.module.sass', '.module.scss', '.module.css', '.png', '.svg', '.gif', '.jpeg', '.jpg' ]
+  }
+}
+
+// TODO: Should be removed when upgrading to react 18
+// https://github.com/reactjs/react-rails#getting-warning-for-cant-resolve-react-domclient-in-react--18
+const ignoreWarningsConfig = {
+  ignoreWarnings: [/Module not found: Error: Can't resolve 'react-dom\/client'/],
+};
+
+module.exports = merge({}, webpackConfig, options, ignoreWarningsConfig)
