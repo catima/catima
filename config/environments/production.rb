9ef4421c4eae005ba7093f00c457c31fd1fd0584
Luca Guindani <luca.guindani@gmail.com>
2023-11-20 14:58:00 +0100

    Add comment for STDOUT logger in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,10 +109,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Use a different logger for distributed setups.
-  # require "syslog/logger"
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
-
+  # If the RAILS_LOG_TO_STDOUT envar is set, log to STDOUT instead of a file.
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
