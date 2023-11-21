3d7e82100b2734d3db3a46a1e949572b245322d3
Luca Guindani <luca.guindani@gmail.com>
2023-11-21 11:06:51 +0100

    Fix redis configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:#{ENV.fetch('REDIS_PORT', '6379/0')}" }
+  config.redis = {
+    url: "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:#{ENV.fetch('REDIS_PORT', '6379')}/#{ENV.fetch('REDIS_DB', '0')}"
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:#{ENV.fetch('REDIS_PORT', '6379/0')}" }
+  config.redis = {
+    url: "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:#{ENV.fetch('REDIS_PORT', '6379')}/#{ENV.fetch('REDIS_DB', '0')}"
+  }
 end
