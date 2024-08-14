c7c65442dfa345367902d8bd4fe8aa498557ee18
Luca Guindani <luca.guindani@gmail.com>
2024-08-14 14:32:11 +0200

    Fix tests

--- test/integration/catalog_admin/fields_components_test.rb
+++ test/integration/catalog_admin/fields_components_test.rb
@@ -35,6 +35,6 @@ class CatalogAdmin::FieldsComponentsTest < ActionDispatch::IntegrationTest
     visit("/multilingual/en/admin/books/new")
     click_on('Create Book')
 
-    assert(page.has_content?("<td colspan=\"2\">can't be blank</td>"))
+    assert(page.has_content?("can't be blank"))
   end
 end
