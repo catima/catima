ce34b37f45b654156a0b4872c120a6bbfa1bb641
David M <davidmiserez@gmail.com>
2024-11-06 08:23:05 +0000

    Revert "Fix reference field creaiton in category"
    This reverts commit bb47e4499dc31c9bba301dc6986fd42faeaef3f7.
    
    It appears that reference field in categories was used in the past and
    working. This commit [1] introduced the bug that prevent them to be
    created.
    
    Reverting this commit and fixing the bug introduced instead.
    
    [1]: fb72572a37adca8c6557918467203e4e6e8bd707

--- app/helpers/field_sets_helper.rb
+++ app/helpers/field_sets_helper.rb
@@ -11,10 +11,4 @@ module FieldSetsHelper
 
     "(#{I18n.t('plural').downcase}: “#{field_set.name_plural}”, #{I18n.t('slug').downcase}: “#{field_set.slug}”)"
   end
-
-  def field_set_type_choices(field_set)
-    type_choices = Field.type_choices
-    type_choices = type_choices.reject { |choice| choice.first == 'reference' } if field_set.is_a?(Category)
-    type_choices
-  end
 end
