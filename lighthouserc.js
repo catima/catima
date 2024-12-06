06e36801ca60aa4521aac9f5766a2a6139cfc08f
David M <davidmiserez@gmail.com>
2024-12-06 14:48:47 +0000

    Add indexation for tested seo catalog

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,6 +3,7 @@ module.exports = {
     collect: {
       numberOfRuns: 3,
       url: [
+        'http://localhost:3001/one/en',
         'http://localhost:3001/one/en/authors',
         'http://localhost:3001/one/en/authors/525911936-author-with-images'
       ],
