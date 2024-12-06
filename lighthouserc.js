aabda69df9b78216c5a54b96d5b513f41f6c4b21
David M <davidmiserez@gmail.com>
2024-12-06 14:19:21 +0000

    Add Lighthouse Github Actions integration
    Reports are uploaded as gha artifacts.

---
+++ lighthouserc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+      url: [
+        'http://localhost:3001/one/en/authors',
+        'http://localhost:3001/one/en/authors/525911936-author-with-images'
+      ],
+      startServerCommand: 'rails server -p 3001',
+    },
+    upload: {
+      target: 'filesystem',
+      outputDir: 'lhci-reports',
+    },
+  },
+};
