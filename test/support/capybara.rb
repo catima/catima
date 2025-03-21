473f2fdaae7278d217b10e65bef99274c143f52a
root <luca.guindani@gmail.com>
2025-03-21 16:05:33 +0000

    Use chrome v132 for ci tests

--- test/support/capybara.rb
+++ test/support/capybara.rb
@@ -15,7 +15,7 @@ else
 end
 
 def driver_params
-  arguments = %w[disable-gpu no-sandbox]
+  arguments = %w[disable-gpu]
   arguments.push("headless") unless ENV['HEADLESS'] == "0"
   {
     options: Selenium::WebDriver::Chrome::Options.new(
