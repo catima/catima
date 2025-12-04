c7f2ad11c8510b65b28435f9cd45d0014168473c
Luca Guindani <luca.guindani@gmail.com>
2025-12-04 11:27:29 +0100

    Fix directory path in k8s-poststart.sh and update permissions for mounted volumes

--- docker/config/k8s-poststart.sh
+++ docker/config/k8s-poststart.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 if [ "$(pwd)" != "/var/www/catima" ]; then
-  cd /var/www/impact || { echo "Failed to change directory to /var/www/catima"; exit 1; }
+  cd /var/www/catima || { echo "Failed to change directory to /var/www/catima"; exit 1; }
 fi
+
+# Ensure mounted volumes have correct permissions
+chown -R catima:catima public
+chown -R catima:catima log
+chown -R catima:catima exports
 
 # Notify BugSnag about the deployment
 rake bugsnag:deploy \
