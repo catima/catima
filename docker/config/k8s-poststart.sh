3208471fbc8185f2e7fcaf31f9d58dea35af5594
Luca Guindani <luca.guindani@gmail.com>
2025-12-04 15:22:12 +0100

    Remove redundant permission changes for mounted volumes in k8s-poststart.sh

--- docker/config/k8s-poststart.sh
+++ docker/config/k8s-poststart.sh
@@ -4,11 +4,6 @@ if [ "$(pwd)" != "/var/www/catima" ]; then
   cd /var/www/catima || { echo "Failed to change directory to /var/www/catima"; exit 1; }
 fi
 
-# Ensure mounted volumes have correct permissions
-chown -R catima:catima public
-chown -R catima:catima log
-chown -R catima:catima exports
-
 # Notify BugSnag about the deployment
 rake bugsnag:deploy \
   BUILDER="Deployer"
