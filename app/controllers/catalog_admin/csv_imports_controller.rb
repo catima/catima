db430ca392e1348a6cb3d9d11c3d75982e1f4671
David M. <david.miserez@unil.ch>
2026-03-10 07:29:56 +0100

    Improve CSV import encoding (#649)
    * Fix import csv auto detect encoding problem
    
    * Improve import csv auto detect encoding
    
    * Limit memory consumption for encoding detection
    
    * Remove unused variable
    
    * Run utf-16 detection before utf-8
    
    * Add tests for import CSV

--- app/controllers/catalog_admin/csv_imports_controller.rb
+++ app/controllers/catalog_admin/csv_imports_controller.rb
@@ -36,7 +36,7 @@ class CatalogAdmin::CSVImportsController < CatalogAdmin::BaseController
     @csv_import = CSVImport.new(params) do |import|
       import.creator = current_user
       import.item_type = item_type
-      import.file_encoding = "detect"
+      import.file_encoding = "detect" if params.nil?
     end
   end
 
