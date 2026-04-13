270f88eed9b27d99d5b54b40f90d20380bd1a916
David M. <davidmiserez@gmail.com>
2026-04-13 12:48:49 +0000

    Add tests

--- test/models/item_list/references_test.rb
+++ test/models/item_list/references_test.rb
@@ -12,6 +12,27 @@ class ItemList::ReferencesTest < ActiveSupport::TestCase
     assert_includes(results.to_a, child)
   end
 
+  test "referencing items are sorted by primary field (name) ascending" do
+    parent = items(:one_author_very_old)
+
+    child1 = items(:one_author_stephen_king) # name: "Stephen King"
+    child2 = items(:one_author_very_first)   # name: "AAA / Very first author"
+
+    child1.data["one_author_collaborator_uuid"] = parent.id
+    child1.save!
+    child2.data["one_author_collaborator_uuid"] = parent.id
+    child2.save!
+
+    field = fields(:one_author_collaborator)
+    references = ItemList::References.new(:item => parent, :field => field)
+    results = references.items.to_a
+
+    assert_equal 2, results.size
+    # "AAA / Very first author" < "Stephen King" alphabetically
+    assert_equal child2, results.first
+    assert_equal child1, results.last
+  end
+
   private
 
   def parent_and_child_authors
